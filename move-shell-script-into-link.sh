cfdd1316075c68c209c93233b78ac155de7f19a9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-07-31 20:56:05 +0200

    Modify the script to manage some other useful usecases.

--- move-shell-script-into-link.sh
+++ move-shell-script-into-link.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-DEST_DIR=~/bin
+# Filter files
+#FILTER=*.sh
+FILTER=.*
+
+# Directory structure
+DEST_DIR=~
 SRC_DIR=/home/tony/Dropbox/repositories/git/slash-my-home-slash-bin
 
-for i in $(ls $SRC_DIR *.sh); 
+for i in $(ls $SRC_DIR $FILTER); 
 do
     echo "rm $DEST_DIR/$i"
     rm $DEST_DIR/$i
