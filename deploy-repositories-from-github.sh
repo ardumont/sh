6be358e3abed6bc749be5ca5f6416bbd2e086ae7
tony <tony@dagobah.(none)>
2011-08-13 13:10:38 +0200

    Keep a script uptodate with the last repositories.

---
+++ deploy-repositories-from-github.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ~/Dropbox/repositories/git/
+
+git clone git@github.com:ardumont/my-org-files.git
+git clone git@github.com:ardumont/reading-pro-git.git
+git clone git@github.com:ardumont/fest-assert-goodies.git
+git clone git@github.com:ardumont/slash-my-home-slash-bin.git
+git clone git@github.com:ardumont/stumpwm-config.git
+git clone git@github.com:ardumont/my-mockito-lab.git
+git clone git@github.com:ardumont/fest-assert-goodies.git
