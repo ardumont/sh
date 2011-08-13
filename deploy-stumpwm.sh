e84d7c8d6da36845075b8bb2f4d4a19e9a65fe2e
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-08-13 14:40:26 +0200

    Create a script to fully install stumpwm.

---
+++ deploy-stumpwm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install stumpwm
+sudo apt-get install stumpwm
+
+# Create the file
+sudo touch /usr/share/xsessions/stumpwm.desktop
+
+# Add the entry for gdm
+(cat <<EOF
+[Desktop Entry]
+        Exec=stumpwm
+        TryExec=stumpwm
+        Name=stumpwm
+        Comment=The Mouseless Window Manager!
+EOF
+)> stumpwm.buffer
+
+sudo cp stumpwm.buffer /usr/share/xsessions/stumpwm.desktop
+
+rm stumpwm.buffer
