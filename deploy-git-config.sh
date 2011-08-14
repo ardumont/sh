4e0d0e691d1da07ba3339454a775c1088a791940
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-08-14 19:49:10 +0200

    Update some setup for git.

--- deploy-git-config.sh
+++ deploy-git-config.sh
@@ -2,3 +2,6 @@
 
 git config --global user.name "Antoine R. Dumont"
 git config --global user.email "antoine.romain.dumont@gmail.com"
+
+git config --global core.editor "emacs -nw"
+git config color.ui true
