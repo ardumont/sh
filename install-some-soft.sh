d2e930ed2145632b25a11cef88b3c9f535f1fdfb
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-08-14 18:39:28 +0200

    Create a script to reference once and for all the soft you use. This way, for the next install, just launch this script.

---
+++ install-some-soft.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# those we can install directly
+sudo apt-get install maven2 git gitk chromium-browser
+
+# depending on the machine 
+# sudo apt-get install linux-generic-pae
+
+# the other one we can't
+# conkeror
+# chrome
