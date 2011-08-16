2c5937fd1d18cbe8e6faf34be01afce83ce9fef4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-08-16 19:06:31 +0200

    Script to dump a mysql db dump.

---
+++ dump-db.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Use : $0 <database> <file>";
+  exit 1;
+fi
+
+_set(){
+  MYSQLDUMP=mysqldump
+  USER=root
+}
+
+_unset(){
+  unset MYSQLDUMP USER
+}
+
+_set
+
+${MYSQLDUMP} -u ${USER} -p $1 > $2
+
+_unset
