839b1f9ca0891b68443335c5eb8061f456fe9234
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-08-16 18:56:58 +0200

    Script to delete useless files of backup and other things.

---
+++ clean-noise.sh
@@ -0,0 +1,25 @@
+ #!/bin/bash
+
+_set(){
+	#commands
+	FIND=locate
+	#files or folder to delete
+	noise="Thumbs.db .DS_Store *~ .*~"
+}
+
+_unset(){
+	unset FIND noise
+}
+
+_set
+
+echo "Delete '${noise}' ..."
+for i in $($FIND $noise); do
+    # directory
+    [ -d $i ] && echo "rm -r $i" && rm -r $i
+    # file but not a link
+    [ -f $i ] && echo "rm $i" && rm $i
+done
+echo "Delete '${noise}' done!"
+
+_unset
