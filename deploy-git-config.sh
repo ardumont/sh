4e19271a505704dd76bb0b3d1852ccb2c2c76131
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-08-17 15:32:04 +0200

    Change the tool to merge kdiff3 by ediff.

--- deploy-git-config.sh
+++ deploy-git-config.sh
@@ -6,4 +6,4 @@ git config --global user.email "antoine.romain.dumont@gmail.com"
 git config --global core.editor "emacs -nw"
 git config --global color.ui true
 
-git config merge.tool kdiff3
+git config --global merge.tool ediff
