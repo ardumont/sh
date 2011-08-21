fec3b826a4703812f996f69217f471ff4fa5def7
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-08-21 15:49:19 +0200

    A script to create a user and a database associated with it.

---
+++ create-postgresql-user-and-db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# must be launched by the user postgres
+
+[ -z $1 -o -z $2 ] && echo "Use: create-postgresql-user-and-db.sh <username> <dbname>" && exit 1;
+
+$USERNAME=$1
+$DBNAME=$2
+
+echo "createuser -D -A -P $USERNAME"
+createuser -D -A -P $USERNAME
+
+echo "createdb -O $USERNAME $DBNAME"
+createdb -O $USERNAME $DBNAME
