db8fd533ad466d4126f60df32a5a18020b517ce1
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-09-04 21:46:43 +0200

    Add some default aliases on git.

--- deploy-git-config.sh
+++ deploy-git-config.sh
@@ -3,7 +3,22 @@
 git config --global user.name "Antoine R. Dumont"
 git config --global user.email "antoine.romain.dumont@gmail.com"
 
+# emacs
 git config --global core.editor "emacs -nw"
+# color everywhere
 git config --global color.ui true
 
+# tool to help the merging
 git config --global merge.tool ediff
+
+# aliases
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+
+# unstage a file
+git config --global alias.unstage 'reset HEAD --'
+
+# see the last log
+git config --global alias.last 'log -1 HEAD'
