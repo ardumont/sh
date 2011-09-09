45f388bf25022554f80d2e697bfe1fa97b148665
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-09-09 20:17:43 +0200

    Add stalonetray, usefull when in stumpwm to call nm-applet to add or update some wifi connection.

--- deploy-soft.sh
+++ deploy-soft.sh
@@ -29,4 +29,4 @@
 # sweethome3d
 aptitude install maven2 git gitk chromium-browser localepurge tree deborphan \
     language-pack-en language-pack-fr gparted stalonetray postgresql pgadmin3 \
-    phppgadmin mysql-server sun-java6-jdk sun-java6-source sweethome3d
+    phppgadmin mysql-server sun-java6-jdk sun-java6-source sweethome3d stalonetray
