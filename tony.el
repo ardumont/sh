ce74adb2e80ac5be793d18d6af3179442ffe3afe
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-09-10 19:46:35 +0200

    Init script for emacs with some specific setup for clojure and org-mode.

---
+++ tony.el
@@ -0,0 +1,47 @@
+;; =============================================================================
+;; Clojure config
+;; =============================================================================
+
+(require 'clojure-mode)
+(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+(require 'midje-mode)
+(add-hook 'clojure-mode-hook 'midje-mode)
+
+;; =============================================================================
+;; Org config
+;; =============================================================================
+
+;; display column number
+
+(column-number-mode)
+
+;; org dir
+
+(setq org-directory "~/org")
+
+;; indentation
+
+(setq org-startup-indented t)
+
+;; log work done
+
+(setq org-log-done 'time)
+
+;; notes directory
+
+(setq org-default-notes-file (concat org-directory "/notes.org"))
+
+;; notes binding
+
+(define-key global-map "\C-cc" 'org-capture)
+
+;; tags
+
+(setq org-tag-alist '(("howTo" . ?h)
+                      ("tech" . ?t)
+                      ("emacs" . ?e)
+                      ("orgMode" . ?o)
+                      ("faq" . ?f)
+                      ("firefox")
+                      ("conkeror")
+                      ("linux")))
