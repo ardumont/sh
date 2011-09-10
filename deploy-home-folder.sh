4ccedc24a2065de07ed83da3eda487561011d3c4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-09-10 20:32:48 +0200

    Do some refactoring of the bash shell scripts.

--- deploy-home-folder.sh
+++ deploy-home-folder.sh
@@ -2,19 +2,7 @@
 
 SRC=~/Dropbox/repositories/git
 
-# To destroy a link
-function destroy-link() {
-    [ -h $1 -o -f $1 ] && rm $1
-}
-
-# To destroy a link or backup a directory (use: emacs)
-function destroy-link-or-backup-file() {
-    if [ -h $1 ]; then
-	rm $1
-    else
-	mv $1. "$1.clean-install"
-    fi
-}
+. standard-functions.sh
 
 # ~/bin
 destroy-link ~/bin
--- standard-functions.sh
+++ standard-functions.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-# function to log the action                                                                                                                                                                                                     
-do-action() {                                                                                                                                                                                                                    
-    echo -e "\n$1"                                                                                                                                                                                                               
-    $1                                                                                                                                                                                                                           
+# function to log the action
+do-action() {
+    echo -e "\n$1"
+    $1
+}
+                                                                                      
+# To destroy a link
+function destroy-link() {
+    [ -h $1 -o -f $1 ] && rm $1
+}
+
+# To destroy a link or backup a directory (use: emacs)
+function destroy-link-or-backup-file() {
+    if [ -h $1 ]; then
+	rm $1
+    else
+	mv $1. "$1.clean-install"
+    fi
 }
