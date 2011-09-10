dac2da6412879f20de541e34d6a6a4306e383cdc
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-09-10 19:53:07 +0200

    Simplify the install script for emacs. Add the tony.el link inside the ~/.emacs.d after the first deploying of technomancy's starter kit.

--- deploy-emacs.sh
+++ deploy-emacs.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+. standard-functions.sh
+
 # Install git and emacs
 echo "sudo apt-get install git emacs"
 sudo apt-get install git emacs
 
 # Delete the old link 
-[ -h ~/.emacs.d ] && echo "rm ~/.emacs.d" && rm ~/.emacs.d
+[ -h ~/.emacs.d ] && do-action "rm ~/.emacs.d"
 # or default directory for emacs
-[ -d ~/.emacs.d ] && echo "mv ~/.emacs.d ~/.emacs.d.clean-install" && mv ~/.emacs.d ~/.emacs.d.clean-install
+[ -d ~/.emacs.d ] && do-action "mv ~/.emacs.d ~/.emacs.d.clean-install"
 
 # Then clone the repository of the emacs-starter-kit
 # https://github.com/technomancy/emacs-starter-kit
 
-echo "git clone https://github.com/technomancy/emacs-starter-kit.git ~/.emacs.d"
-git clone https://github.com/technomancy/emacs-starter-kit.git ~/.emacs.d
+do-action "git clone https://github.com/technomancy/emacs-starter-kit.git ~/.emacs.d"
+
+# create a link to my specific setup
+[ -h ~/.emacs.d/tony.el ] && do-action "rm ~/.emacs.d/tony.el"
+do-action "ln -s ~/Dropbox/repositories/git/slash-my-home-slash-bin/tony.el ~/.emacs.d/tony.el"
