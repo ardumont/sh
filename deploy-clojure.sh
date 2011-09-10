c02e2d6c9ce8406a5781c4df30fd76aba00fa2ee
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-09-10 20:32:12 +0200

    Add the cake binary to install of clojure.

--- deploy-clojure.sh
+++ deploy-clojure.sh
@@ -1,22 +1,33 @@
 #!/bin/bash -xe
 
+. standard-functions.sh
+
 # Install clojure
-sudo aptitude install clojure
+do-action "sudo aptitude install clojure"
 
 # deploy tools to help
-[ ! -d ~/repositories ] && mkdir -p ~/repositories/clones
+[ ! -d ~/repositories ] && do-action "mkdir -p ~/repositories/clones"
 cd ~/repositories/clones
 
-[ ! -d ~/repositories/clones/leiningen ] && git clone https://github.com/technomancy/leiningen.git
+[ ! -d ~/repositories/clones/leiningen ] && do-action "git clone https://github.com/technomancy/leiningen.git" || git reset --hard HEAD && git pull
 
 # will launch the download of all needed
 cd leiningen
-lein
+do-action "lein"
 
 # install the plugin to make a server swank
-lein plugin install swank-clojure 1.3.1
+do-action "lein plugin install swank-clojure 1.3.1"
 
+# FIXME find a way to install plugin emacs offline
 # Install clojure mode in emacs
 # M-x list-packages
 # select clojure-mode I
 # x to install
+
+# install cake
+[ ! -d ~/repositories/clones/cake ] && do-action "git clone git://github.com/ninjudd/cake.git" || git reset --hard HEAD && git pull
+
+# create a link
+destroy-link ~/repositories/clones/cake/bin/cake
+ln -s ~/repositories/clones/cake/bin/cake ~/bin/cake
+
