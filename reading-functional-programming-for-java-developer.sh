1af93e1e5027ac95566a39b8fa0d57aa447a24da
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-09-15 22:02:45 +0200

    Launch the pdf about functional programming for java developer.

---
+++ reading-functional-programming-for-java-developer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+evince /home/tony/Dropbox/Documents/lecture/Oreilly.Functional.Programming.for.Java.Developers.Jul.2011/Oreilly.Functional.Programming.for.Java.Developers.Jul.2011.pdf
