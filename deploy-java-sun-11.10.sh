8979c47b5574aef796d6a0e7c9142fdf9a419898
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-10-16 18:01:09 +0200

    Update on scripts for deploying java.

--- deploy-java-sun-11.10.sh
+++ deploy-java-sun-11.10.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Add the repository
-sudo add-apt-repository "deb http://archive.canonical.com/ubuntu natty partner"
+sudo add-apt-repository ppa:ferramroberto/java
 
 # Update the repository
 sudo aptitude update
 
 # Install sun
-yes | aptitude install sun-java6-jdk sun-java6-source
+yes | aptitude install sun-java6-jdk sun-java6-source sun-java6-plugin
 
 # Once install choose the sun-jre ad the default one
 update-alternatives --config java
