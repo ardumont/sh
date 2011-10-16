eee353967b477e6304de2ca467fc03493d537d72
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-10-16 18:47:25 +0200

    Gnome is not the window manager by default and i really dislike unity so script the install process.

---
+++ deploy-gnome.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+# gnome-shell -> gnome 3
+# gnome-session-fallback -> session gnome fallback from ubuntu 11.04
+# gnome-tweak-tool -> menu to tweak gnome
+
+sudo aptitude install gnome-shell gnome-session-fallback gnome-tweak-tool
