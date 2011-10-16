6dc03b1e504fedbcfcd5daa36071ac2ace63d4bf
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-10-16 18:46:54 +0200

    Refactor the script to install conkeror. Conkeror is not present in ubuntu 11.04 nor 11.10. Script it.

--- deploy-conkeror.sh
+++ deploy-conkeror.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 DEST=~/Downloads/conkeror
-URL=http://launchpadlibrarian.net/53257451/conkeror_0.9.2%2Bgit100804-1ubuntu1_all.deb
+URL_CONKEROR=http://launchpadlibrarian.net/53257451/conkeror_0.9.2%2Bgit100804-1ubuntu1_all.deb
+URL_XULRUNNER=http://launchpadlibrarian.net/70321863/xulrunner-1.9.2_1.9.2.17%2Bbuild3%2Bnobinonly-0ubuntu1_i386.deb
+
+FILE_CONKEROR=conkeror_0.9.2+git100804-1ubuntu1_all.deb
+FILE_XULRUNNER=xulrunner-1.9.2_1.9.2.17+build3+nobinonly-0ubuntu1_i386.deb
 
 # does the directory exist?
 if [ ! -d $DEST ];
@@ -11,35 +15,42 @@ then
     mkdir -p $DEST
 fi
 
-# go inside it
-echo "cd $DEST"
-cd $DEST
+# install the file
+function install-soft() {
+    file_soft=$1
+    package_soft=$2
+    cd $DEST
+    dpkg -l | grep $package_soft
+    if [ $? != 0 ];
+    then
+        echo "$package_soft is not installed! Install it!.".
+        # Install
+        sudo dpkg -i $file_soft
+        echo "Installation done!"
+    else
+        echo "$package_soft already installed, nothing to do!"
+    fi
+}
 
-# Is the file already downloaded?
-if [ ! -f conkeror_0.9.2+git100804-1ubuntu1_all.deb ];
-then
-    echo "Download conkeror."
-    # download it
-    wget $URL
-fi
+# download the soft
+function dl-soft() {
+    file_soft=$1
+    package_soft=$2
+    cd $DEST
+    # Is the file already downloaded?
+    if [ ! -f $file_soft ];
+    then
+        echo "Download $2."
+        # download it
+        wget $URL
+    fi
+}
 
-if [ ! -f xulrunner-1.9.2_1.9.2.17%2Bbuild3%2Bnobinonly-0ubuntu1_i386.deb ];
-then
-    echo "Download xulrunner."
-    wget http://launchpadlibrarian.net/70321863/xulrunner-1.9.2_1.9.2.17%2Bbuild3%2Bnobinonly-0ubuntu1_i386.deb
-fi
+# Download or not
+dl-soft $FILE_XULRUNNER $URL_XULRUNNER
+dl-soft $FILE_CONKEROR $URL_CONKEROR
 
 # Is the soft already installed?
-dpkg -l | grep conkeror
-if [ $? != 0 ];
-then
-    echo "Conkeror is not installed! Install conkeror.".
-    # dependency on xulrunner 192
-#    sudo apt-get install xulrunner-1.9.2
-    sudo dpkg -i xulrunner-1.9.2_1.9.2.17%2Bbuild3%2Bnobinonly-0ubuntu1_i386.deb
-    # Install
-    sudo dpkg -i conkeror_0.9.2+git100804-1ubuntu1_all.deb
-    echo "Installation done!"
-else
-    echo "Conkeror already installed, nothing to do!"
-fi
+install-soft $FILE_XULRUNNER xulrunner
+install-soft $FILE_CONKEROR conkeror
+
