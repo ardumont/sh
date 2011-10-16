e5b335ba34509d8bdc1283b75c73745ee5962b3f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-10-16 17:38:04 +0200

    Add the canonical partner repository.

--- deploy-java-sun.sh
+++ deploy-java-sun.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Add the repository
-# TODO
+#sudo add-apt-repository "deb http://archive.canonical.com/ubuntu natty partner"
+sudo add-apt-repository "deb http://archive.canonical.com/ubuntu oneiric partner"
 
 # Update the repository
 sudo aptitude update
 
 # Install sun
-sudo aptitude install sun-java6-jdk sun-java6-source
+yes | aptitude install sun-java6-jdk sun-java6-source
 
 # Once install choose the sun-jre ad the default one
-sudo update-alternatives --config java
+update-alternatives --config java
