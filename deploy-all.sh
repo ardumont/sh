f5e791aeb196380d2660ac6c2922e8278af944ee
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-10-22 09:56:05 +0200

    A script to deploy them all.

---
+++ deploy-all.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+WDIR=$(dirname $0)
+VERSION=$1
+WITH_JAVA=$2
+
+[ -z $VERSION ] && VERSION=11.10
+[ -z $WITH_JAVA ] && $WITH_JAVA=n
+
+# Deploy the basename soft
+$WDIR/deploy-soft.sh
+
+# Deploy home
+$WDIR/deploy-home-folder.sh
+
+# Deploy git config
+$WDIR/deploy-git-config.sh
+
+# Deploy emacs
+#$WDIR/deploy-emacs.sh -> need to be finished
+
+# Deploy stumpwm
+$WDIR/deploy-stumpwm.sh
+
+# Deploy conkeror
+$WDIR/deploy-conkeror.sh
+
+# Deploy byobu
+$WDIR/deploy-byobu.sh
+
+# Deploy clojure
+$WDIR/deploy-clojure.sh
+
+# Install java if asked
+[ $WITH_JAVA = "y" ] && [ -f $WDIR/deploy-java-sun-$VERSION.sh ] && $WDIR/deploy-java-sun-$VERSION.sh
+
+# Deploy gnome on 11.10
+[ $VERSION = "11.10" ] && [ -f $WDIR/deploy-gnome-$VERSION.sh ] && $WDIR/deploy-gnome-$VERSION.sh
