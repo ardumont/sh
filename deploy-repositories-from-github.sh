a57376cac02be14041c92109c4413ea6c794feb4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-10-22 09:41:32 +0200

    Update the list of github repositories.

--- deploy-repositories-from-github.sh
+++ deploy-repositories-from-github.sh
@@ -10,3 +10,6 @@ git clone git@github.com:ardumont/stumpwm-config.git
 git clone git@github.com:ardumont/my-mockito-lab.git
 git clone git@github.com:ardumont/fest-assert-goodies.git
 git clone git@github.com:ardumont/conkeror-saner-defaults.git
+git clone git@github.com:ardumont/my-project-euler-lab.git
+git clone git@github.com:ardumont/my-node-js-lab.git
+git clone git@github.com:denlab/clojure-plays-checkers.git
