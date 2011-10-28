9fb4bbfa6e6c8a7f19221311a5c413a8b3ba893f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-10-28 19:14:15 +0200

    Make the script to deploy clojure work out of the box!

--- deploy-clojure.sh
+++ deploy-clojure.sh
@@ -1,41 +1,37 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
-. standard-functions.sh
+. $(dirname $0)/standard-functions.sh
 
 # Install clojure
 do-action "sudo aptitude install clojure inotify-tools ruby"
 
-# deploy tools to help
-[ ! -d ~/repositories ] && do-action "mkdir -p ~/repositories/clones"
-cd ~/repositories/clones
+WDIR=$HOME/repositories/clones
 
-[ ! -d ~/repositories/clones/leiningen ] && do-action "git clone https://github.com/technomancy/leiningen.git ~/repositories/clones/leiningen" || git reset --hard HEAD && git pull
+# deploy tools to help
+[ ! -d $HOME/repositories ] && do-action "mkdir -p $WDIR"
+cd $WDIR
+
+deploy-git-clone-or-update https://github.com/technomancy/leiningen.git $WDIR/leiningen
 
 # will launch the download of all needed
-cd leiningen
+cd $WDIR/leiningen
 do-action "lein"
 
 # install the plugin to make a server swank
 do-action "lein plugin install swank-clojure 1.3.1"
 
-# FIXME find a way to install plugin emacs offline
-# Install clojure mode in emacs
-# M-x list-packages
-# select clojure-mode I
-# x to install
-
 # install cake
-deploy-git-clone-or-update git://github.com/flatland/cake.git ~/repositories/clones/cake
-destroy-link ~/bin/cake
-ln -s ~/repositories/clones/cake/bin/cake ~/bin/cake
+deploy-git-clone-or-update git://github.com/flatland/cake.git $WDIR/cake
+destroy-link $HOME/bin/cake
+do-action "ln -s $WDIR/cake/bin/cake $HOME/bin/cake"
 
 # install midje-cake
-deploy-git-clone-or-update git://github.com/marick/Midje.git ~/repositories/clones/Cake-midje
+deploy-git-clone-or-update git://github.com/marick/Midje.git $WDIR/Cake-midje
 # retrieve the tasks.clj files from this project and copy it inside your project root
 # add this files to the root of your project 
 # wget https://github.com/marick/Midje/raw/master/examples/cake-midje/tasks.clj
 
 # install faster-cake-autotest
-deploy-git-clone-or-update git://github.com/denlab/faster-cake-autotest.git ~/repositories/clones/faster-cake-autotest
-destroy-link ~/bin/cake-autotest
-ln -s ~/repositories/clones/faster-cake-autotest/cake-autotest ~/bin/cake-autotest
+deploy-git-clone-or-update git://github.com/denlab/faster-cake-autotest.git $WDIR/faster-cake-autotest
+destroy-link $HOME/bin/cake-autotest
+do-action "ln -s $WDIR/faster-cake-autotest/cake-autotest $HOME/bin/cake-autotest"
