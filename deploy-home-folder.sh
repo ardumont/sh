cbd0b7819f76b007f31933110ca7fca85de3ed4c
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-10-28 19:41:06 +0200

    Add the call to the method do-action that log the action before executing it.

--- deploy-home-folder.sh
+++ deploy-home-folder.sh
@@ -1,49 +1,49 @@
-#!/bin/bash
+#!/bin/bash -e
 
 SRC=$HOME/Dropbox/repositories/git
 
-. standard-functions.sh
+. $(dirname $0)/standard-functions.sh
 
-# ~/bin
-destroy-link ~/bin
-ln -s $SRC/slash-my-home-slash-bin ~/bin
+# $HOME/bin
+destroy-link $HOME/bin
+do-action "ln -s $SRC/slash-my-home-slash-bin $HOME/bin"
 
-# ~/org
-destroy-link ~/org
-ln -s $SRC/my-org-files ~/org
+# $HOME/org
+destroy-link $HOME/org
+do-action "ln -s $SRC/my-org-files $HOME/org"
 
-# ~/.stumpwmrc
-destroy-link ~/.stumpwmrc
-ln -s $SRC/stumpw-config/.stumpwmrc ~
+# $HOME/.stumpwmrc
+destroy-link $HOME/.stumpwmrc
+do-action "ln -s $SRC/stumpw-config/.stumpwmrc $HOME"
 
-# ~/.bash*
-destroy-link ~/.bashrc
-destroy-link ~/.bashrc-work
-destroy-link ~/.bash_aliases
-destroy-link ~/.profile
-destroy-link ~/.bash_history
-destroy-link ~/.bash-prompt
+# $HOME/.bash*
+destroy-link $HOME/.bashrc
+destroy-link $HOME/.bashrc-work
+destroy-link $HOME/.bash_aliases
+destroy-link $HOME/.profile
+destroy-link $HOME/.bash_history
+destroy-link $HOME/.bash-prompt
 
-ln -s $SRC/slash-my-home-slash-bin/.bashrc ~/
-ln -s $SRC/slash-my-home-slash-bin/.bashrc-work ~/
-ln -s $SRC/slash-my-home-slash-bin/.profile ~/
-ln -s $SRC/slash-my-home-slash-bin/.bash_aliases ~/
-ln -s $SRC/slash-my-home-slash-bin/.bash-prompt ~/
+do-action "ln -s $SRC/slash-my-home-slash-bin/.bashrc $HOME/"
+do-action "ln -s $SRC/slash-my-home-slash-bin/.bashrc-work $HOME/"
+do-action "ln -s $SRC/slash-my-home-slash-bin/.profile $HOME/"
+do-action "ln -s $SRC/slash-my-home-slash-bin/.bash_aliases $HOME/"
+do-action "ln -s $SRC/slash-my-home-slash-bin/.bash-prompt $HOME/"
 # not synchronized on github
-ln -s ~/Dropbox/.bash_history ~/
+do-action "ln -s $HOME/Dropbox/.bash_history $HOME/"
 
 # git-bash-completion
-destroy-link ~/.git-completion.bash
-ln -s $SRC/slash-my-home-slash-bin/.git-completion.bash ~/
+destroy-link $HOME/.git-completion.bash
+do-action "ln -s $SRC/slash-my-home-slash-bin/.git-completion.bash $HOME/"
 
 # For conkeror
-destroy-link ~/.conkerorrc
-ln -s $SRC/conkeror-saner-defaults/.conkerorrc ~/
+destroy-link $HOME/.conkerorrc
+do-action "ln -s $SRC/conkeror-saner-defaults/.conkerorrc $HOME/"
 
 # link to some reading
-destroy-link ~/lecture
-ln -s ~/Dropbox/Documents/lecture ~
+destroy-link $HOME/lecture
+do-action "ln -s $HOME/Dropbox/Documents/lecture $HOME"
 
 # link to my perso repositories
-destroy-link ~/repositories/perso
-ln -s ~/Dropbox/repositories/git/ ~/repositories/perso
+destroy-link $HOME/repositories/perso
+do-action "ln -s $HOME/Dropbox/repositories/git/ $HOME/repositories/perso"
