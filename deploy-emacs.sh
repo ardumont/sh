cc8001d06af29769e93f28267ba79dc81e1dc8dd
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-10-28 16:53:36 +0200

    Improve the emacs deploying script.

--- deploy-emacs.sh
+++ deploy-emacs.sh
@@ -4,18 +4,23 @@
 
 # Install git and emacs
 echo "sudo apt-get install git emacs"
-sudo apt-get install git emacs
+sudo aptitude install git emacs
 
 # Delete the old link 
-[ -h ~/.emacs.d ] && do-action "rm ~/.emacs.d"
+[ -h $HOME/.emacs.d ] && do-action "rm $HOME/.emacs.d"
 # or default directory for emacs
-[ -d ~/.emacs.d ] && do-action "mv ~/.emacs.d ~/.emacs.d.clean-install"
+[ -d $HOME/.emacs.d ] && do-action "mv $HOME/.emacs.d $HOME/.emacs.d.clean-install"
 
 # Then clone the repository of the emacs-starter-kit
 # https://github.com/technomancy/emacs-starter-kit
 
-do-action "git clone https://github.com/technomancy/emacs-starter-kit.git ~/.emacs.d"
+do-action "git clone https://github.com/technomancy/emacs-starter-kit.git $HOME/.emacs.d"
 
 # create a link to my specific setup
-[ -h ~/.emacs.d/tony.el ] && do-action "rm ~/.emacs.d/tony.el"
-do-action "ln -s ~/Dropbox/repositories/git/slash-my-home-slash-bin/tony.el ~/.emacs.d/tony.el"
+[ -h $HOME/.emacs.d/tony.el ] && do-action "rm $HOME/.emacs.d/tony.el"
+do-action "ln -s $HOME/Dropbox/repositories/git/slash-my-home-slash-bin/tony.el $HOME/.emacs.d/tony.el"
+
+[ -h $HOME/.emacs.d/linum.el ] && do-action "rm $HOME/.emacs.d/linum.el"
+do-action "wget http://web.student.tuwien.ac.at/$HOMEe0225855/linum/linum.el --output-document $HOME/Dropbox/repositories/git/slash-my-home-slash-bin/linum.el"
+do-action "ln -s $HOME/Dropbox/repositories/git/slash-my-home-slash-bin/linum.el $HOME/.emacs.d/linum.el"
+
