cac4e82bd15dc64ba3c237804daa3dae40298413
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-11-12 23:33:41 +0100

    Script to deploy emacs24.

--- deploy-emacs24.sh
+++ deploy-emacs24.sh
@@ -24,18 +24,18 @@ do-action "sudo aptitude install git emacs-snapshot-gtk"
 # or default directory for emacs
 [ -d $HOME/.emacs.d ] && do-action "mv $HOME/.emacs.d $HOME/.emacs.d.clean-install"
 
-# Then clone the repository of the emacs-starter-kit
-# https://github.com/technomancy/emacs-starter-kit
-do-action "git clone https://github.com/technomancy/emacs-starter-kit.git $HOME/.emacs.d"
-# at the moment, we are with the version 1 of the starter kit as we still depends on 
-do-action "cd $HOME/.emacs.d" # twice else it does not work
+do-action "cd $HOME/.emacs.d"
 
-# create a link to my specific setup
-#[ -h $HOME/.emacs.d/tony.el ] && do-action "rm $HOME/.emacs.d/tony.el"
+# create a link to my specific setup (init.el that loads the starterkit)
+[ -h $HOME/.emacs.d/init.el ] && do-action "rm $HOME/.emacs.d/init.el"
+do-action "ln -s $REPOSITORY/init24.el $HOME/.emacs.d/init.el"
+
+# create a link to my specific setup (tony.el)
+[ -h $HOME/.emacs.d/tony.el ] && do-action "rm $HOME/.emacs.d/tony.el"
 do-action "ln -s $REPOSITORY/tony.el $HOME/.emacs.d/tony.el"
 
 # add some mode not yet available with marmalade
-#[ -h $HOME/.emacs.d/linum.el ] && do-action "rm $HOME/.emacs.d/linum.el"
+[ -h $HOME/.emacs.d/linum.el ] && do-action "rm $HOME/.emacs.d/linum.el"
 do-action "wget http://web.student.tuwien.ac.at/~e0225855/linum/linum.el --output-document $HOME/bin/linum.el"
 do-action "ln -s $HOME/bin/linum.el $HOME/.emacs.d/linum.el"
 
