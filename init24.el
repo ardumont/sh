c1225733f722222c98adaaf9b72d1b4f0b97547f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-11-12 23:31:22 +0100

    Just keep the minimal needed.

--- init24.el
+++ init24.el
@@ -1,14 +1,8 @@
 ;; =============================================================================
-;; env
+;; Emacs starter kit v2
 ;; =============================================================================
 
-(setenv "PATH" (shell-command-to-string "echo $PATH"))
-
-;; =============================================================================
-;; Install needed packages if not present
-;; =============================================================================
-
-; some tricks from the emacs-starter-kit v2
+; repository
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
@@ -18,74 +12,9 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(slime slime-repl clojure-mode midje-mode org auto-complete flymake-shell)
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
-
-;; =============================================================================
-;; Display line number in the left margin
-;; =============================================================================
-
-(global-linum-mode 1)
-
-;; =============================================================================
-;; Autocomplete setup
-;; =============================================================================
-
-;(add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-1.4.20110207/dict")
-(require 'auto-complete-config)
-(ac-config-default)
-
-;; =============================================================================
-;; Clojure config
-;; =============================================================================
-
-(require 'clojure-mode)
-(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
-(require 'midje-mode)
-(add-hook 'clojure-mode-hook 'midje-mode)
-
-; add color into the repl via clojure-jack-in
-(add-hook 'slime-repl-mode-hook 'clojure-mode-font-lock-setup)
-
-;; =============================================================================
-;; Org config
-;; =============================================================================
-
-;; display column number
-
-(column-number-mode)
-
-;; org dir
-
-(setq org-directory "~/org")
-
-;; indentation
-
-(setq org-startup-indented t)
-
-;; log work done
-
-(setq org-log-done 'time)
-
-;; notes directory
-
-(setq org-default-notes-file (concat org-directory "/notes.org"))
-
-;; notes binding
-
-(define-key global-map "\C-cc" 'org-capture)
-
-;; tags
-
-(setq org-tag-alist '(("howTo" . ?h)
-                      ("tech" . ?t)
-                      ("emacs" . ?e)
-                      ("orgMode" . ?o)
-                      ("faq" . ?f)
-                      ("firefox")
-                      ("conkeror")
-                      ("linux")))
