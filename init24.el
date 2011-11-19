039334e0462f2fbc4c870d5208bc22d40c9742a7
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-11-19 23:22:48 +0100

    Add ediff dependency.

--- init24.el
+++ init24.el
@@ -12,7 +12,7 @@
   (package-refresh-contents))
 
 ;; Add in your own as you wish:
-(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-bindings starter-kit-lisp slime slime-repl clojure-mode midje-mode org auto-complete flymake-shell)
+(defvar my-packages '(ediff starter-kit starter-kit-lisp starter-kit-bindings starter-kit-lisp slime slime-repl clojure-mode midje-mode org auto-complete flymake-shell)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
