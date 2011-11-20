dc738f6cabbd9b51b9ccba3d3ebe238a36d6706e
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-11-20 16:22:36 +0100

    Update the script to create or update my repositories.

--- deploy-repositories-from-github.sh
+++ deploy-repositories-from-github.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 . standard-functions.sh
 
@@ -8,3 +8,16 @@ WDIR=$1
 
 cd $WDIR
 
+deploy-git-clone-or-update-soft git@github.com:ardumont/my-org-files.git $WDIR/my-org-files
+deploy-git-clone-or-update-soft git@github.com:ardumont/reading-pro-git.git $WDIR/reading-pro-git
+deploy-git-clone-or-update-soft git@github.com:ardumont/fest-assert-goodies.git $WDIR/fest-assert-goodies
+deploy-git-clone-or-update-soft git@github.com:ardumont/slash-my-home-slash-bin.git $WDIR/slash-my-home-slash-bin
+deploy-git-clone-or-update-soft git@github.com:ardumont/stumpwm-config.git $WDIR/stumpw-config
+deploy-git-clone-or-update-soft git@github.com:ardumont/my-mockito-lab.git $WDIR/my-mockito-lab
+deploy-git-clone-or-update-soft git@github.com:ardumont/conkeror-saner-defaults.git $WDIR/conkeror-saner-defaults
+deploy-git-clone-or-update-soft git@github.com:ardumont/my-project-euler-lab.git $WDIR/my-project-euler-lab
+deploy-git-clone-or-update-soft git@github.com:ardumont/my-node-js-lab.git $WDIR/my-node-js-lab
+deploy-git-clone-or-update-soft git@github.com:ardumont/my-pallet-stevedore-lab.git $WDIR/my-pallet-stevedore-lab
+deploy-git-clone-or-update-soft git@github.com:ardumont/luhnybin.git $WDIR/luhnybin
+deploy-git-clone-or-update-soft git@github.com:denlab/clojure-plays-checkers.git $WDIR/clojure-plays-checkers
+deploy-git-clone-or-update-soft git@github.com:denlab/clojure-dojo-at-lunch.git $WDIR/clojure-dojo-at-lunch
