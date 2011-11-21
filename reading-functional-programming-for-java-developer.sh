0bc659eed29c26eba2fd972314d0b165de36bfa8
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-11-21 18:28:37 +0100

    Delete useless script.

--- reading-functional-programming-for-java-developer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-evince /home/tony/Dropbox/Documents/lecture/Oreilly.Functional.Programming.for.Java.Developers.Jul.2011/Oreilly.Functional.Programming.for.Java.Developers.Jul.2011.pdf
--- reading-practical-clojure.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-evince ~/lecture/clojure/Apress.Practical.Clojure.May.2010/Apress.Practical.Clojure.May.2010.pdf
