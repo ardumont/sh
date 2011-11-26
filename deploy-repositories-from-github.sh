8aaebf5baeed06951c35d96ffcf092452ebccaab
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-11-26 19:02:46 +0100

    Fix typo.

--- deploy-repositories-from-github.sh
+++ deploy-repositories-from-github.sh
@@ -12,7 +12,7 @@ deploy-git-clone-or-update-soft git@github.com:ardumont/my-org-files.git $WDIR/m
 deploy-git-clone-or-update-soft git@github.com:ardumont/reading-pro-git.git $WDIR/reading-pro-git
 deploy-git-clone-or-update-soft git@github.com:ardumont/fest-assert-goodies.git $WDIR/fest-assert-goodies
 deploy-git-clone-or-update-soft git@github.com:ardumont/slash-my-home-slash-bin.git $WDIR/slash-my-home-slash-bin
-deploy-git-clone-or-update-soft git@github.com:ardumont/stumpwm-config.git $WDIR/stumpw-config
+deploy-git-clone-or-update-soft git@github.com:ardumont/stumpwm-config.git $WDIR/stumpwm-config
 deploy-git-clone-or-update-soft git@github.com:ardumont/my-mockito-lab.git $WDIR/my-mockito-lab
 deploy-git-clone-or-update-soft git@github.com:ardumont/conkeror-saner-defaults.git $WDIR/conkeror-saner-defaults
 deploy-git-clone-or-update-soft git@github.com:ardumont/my-project-euler-lab.git $WDIR/my-project-euler-lab
