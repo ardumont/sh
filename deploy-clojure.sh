fc5cb505f14c612b73b66cd0a2245224975d306a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-03 00:05:22 +0100

    Add marginalia as a dependency.

--- deploy-clojure.sh
+++ deploy-clojure.sh
@@ -20,6 +20,7 @@ do-action "lein"
 # install the plugin to make a server swank
 do-action "lein plugin install swank-clojure 1.3.3"
 do-action "lein plugin install lein-midje 1.0.7"
+do-action "lein plugin install lein-marginalia 0.6.1"
 
 # install cake
 # From https://github.com/flatland/cake
