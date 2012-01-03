a6b0a4d6087522f004f1bd1cf00e4f10580130c2
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-03 21:41:52 +0100

    Add the lein noir plugin.

--- deploy-clojure.sh
+++ deploy-clojure.sh
@@ -21,6 +21,7 @@ do-action "lein"
 do-action "lein plugin install swank-clojure 1.3.3"
 do-action "lein plugin install lein-midje 1.0.7"
 do-action "lein plugin install lein-marginalia 0.6.1"
+do-action "lein plugin install lein-noir 1.2.1"
 
 # install cake
 # From https://github.com/flatland/cake
