78c59bb396a3dad66b7bb196ccd661674efe6b8b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-15 09:46:44 +0100

    Create a simple script to retrieve data from 4clojure.

---
+++ retrieve-clojure-data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+# Input the user we want to see
+USER=$1
+[ -z $USER ] && USER=ardumont
+
+# Generate a temp file.
+TMP_FILE=$(mktemp)
+
+#Retrieve the informations around the user we want
+curl http://www.4clojure.com/user/$USER 2>/dev/null > $TMP_FILE
+
+# Check that there is no problem
+CHECK_USER=$(cat $TMP_FILE | grep "User: \w*" -o | head -1 | cut -d' ' -f2)
+
+[ $CHECK_USER != $USER ] && echo "There may be a problem with your input, the user '$USER' may not exist."
+
+# Retrieve the score of this user
+RES=$(cat $TMP_FILE | grep -o ">[0-9]*/[0-9]*<" | sed s/[\<\>]//g)
+
+echo "4clojure.com status - $USER - $RES".
