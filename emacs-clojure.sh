7ea393e3e60db400cfc2f0501ed74a371838f8f3
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-26 19:37:37 +0100

    Add the multi-term.

--- emacs-clojure.sh
+++ emacs-clojure.sh
@@ -3,5 +3,5 @@
 NW=$1
 
 # start emacs
-emacs -fs $NW -e 'clojure-jack-in' -e 'split-window-vertically' -e 'other-window'
+emacs -fs $NW -e 'clojure-jack-in' -e 'split-window-vertically' -e 'other-window' -e 'multi-term'
 
