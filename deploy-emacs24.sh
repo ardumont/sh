526511730cd71d2d9269182795ae22bb36930f8a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-03-01 02:43:48 +0100

    Hack to install on linux mint.

--- deploy-emacs24.sh
+++ deploy-emacs24.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -xe
 
 WDIR=$(dirname $(readlink -f $0))
-source $WDIR/check-ubuntu-version.sh
+#source /etc/lsb-release
+DISTRIB_VERSION=11.10
 
 yes | sudo aptitude install python-software-properties
 sudo add-apt-repository ppa:cassou/emacs
 sudo tee -a /etc/apt/sources.list <<EOF
 
 # For Emacs 24
-deb http://ppa.launchpad.net/cassou/emacs/ubuntu $UBUNTU_VERSION main
-deb-src http://ppa.launchpad.net/cassou/emacs/ubuntu $UBUNTU_VERSION main
+deb http://ppa.launchpad.net/cassou/emacs/ubuntu $DISTRIB_VERSION main
+deb-src http://ppa.launchpad.net/cassou/emacs/ubuntu $DISTRIB_VERSION main
 EOF
 
 # Update since the sources have been modified
