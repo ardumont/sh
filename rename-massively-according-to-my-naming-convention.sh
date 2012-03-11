8ef76d64b1b1b926404c7893ff78eb5aa0f8aae8
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-03-11 09:16:14 +0100

    Script to rename massively files according my naming convention.

---
+++ rename-massively-according-to-my-naming-convention.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+
+DIR=$1
+if [ -z $DIR ]; then
+    echo "Usage: $0 <directory>"
+    exit 1;
+fi
+
+cd $DIR
+
+rename 's/_/-/g' *
+
+for i in $(ls); do
+    cd $DIR/$i
+    rename 's/---/./g' *
+    rename 's/_-_/./g' *
+    rename 's/-_-/./g' *
+    rename 's/ /-/g' *
+    rename 's/_/-/g' *
+done
