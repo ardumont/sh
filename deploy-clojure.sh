d01305de872321908b5509400d7d6442fb01a380
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-03-17 09:47:25 +0100

    Separate script for lein plugin and the install of clojure.

--- deploy-clojure.sh
+++ deploy-clojure.sh
@@ -5,8 +5,5 @@
 # Install clojure
 yes | sudo aptitude install clojure inotify-tools ruby leiningen
 
-# install the plugin to make a server swank
-do-action "lein plugin install swank-clojure 1.3.3"
-do-action "lein plugin install lein-midje 1.0.7"
-do-action "lein plugin install lein-marginalia 0.6.1"
-do-action "lein plugin install lein-noir 1.2.1"
+# Install tools
+$(dirname $0)/deploy-tools-clojure.sh
---
+++ deploy-tools-clojure.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# install the plugin to make a server swank
+lein  plugin install swank-clojure 1.3.4
+lein plugin install lein-midje 1.0.7
+lein plugin install lein-marginalia 0.7.0-SNAPSHOT
+lein plugin install lein-noir 1.2.1
+lein plugin install lein-cljs 0.2.2
+
