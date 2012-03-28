a224bd3c45954ddfb3c821368209fcd67adf8dcd
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-03-28 22:01:42 +0200

    Use a link for the eclipse installation instead of the folder.

--- ide-eclipse.sh
+++ ide-eclipse.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-/home/tony/applications/eclipse-jee-indigo-linux-gtk-x86-gwt-git-maven/eclipse
-#/home/tony/applications/eclipse-jee-helios-SR2-linux-gtk-x86-gwt-maven/eclipse
+~/applications/eclipse/eclipse
