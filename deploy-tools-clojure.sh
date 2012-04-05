dd73b9536cb11b85c9466b01ee7e35694b7528aa
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-04-05 20:41:45 +0200

    Update the lein-marginalia plugin version.

--- deploy-tools-clojure.sh
+++ deploy-tools-clojure.sh
@@ -9,7 +9,7 @@ lein -v
 # install the plugin to make a server swank
 lein plugin install swank-clojure 1.4.0
 lein plugin install lein-midje 1.0.7
-lein plugin install lein-marginalia 0.7.0-SNAPSHOT
+lein plugin install lein-marginalia 0.7.0
 lein plugin install lein-noir 1.2.1
 lein plugin install lein-cljs 0.2.2
 
