71554b9903d5692db70e557ae6578f0e533bb277
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-04-07 21:38:09 +0200

    Add latex.

--- deploy-soft.sh
+++ deploy-soft.sh
@@ -7,7 +7,8 @@ if [ -f /etc/lsb-release ]; then #ubuntu
     sudo aptitude install git gitk chromium-browser localepurge tree deborphan \
         language-pack-en language-pack-fr gparted stalonetray  sweethome3d \
         inotify-tools ruby curl yajl-tools openssh-server sysvbanner most \
-        graphviz audacious audacious-plugins gnuplot vim latex latex-beamer texlive-latex-extra 
+        graphviz audacious audacious-plugins gnuplot vim latex latex-beamer \
+        texlive-latex-extra texlive-fonts-recommended ttf-marvosym
 
     [ $DISTRIB_RELEASE = "11.10" ] && sudo aptitude install nautilus-dropbox gtalk meld dstat htop
 
