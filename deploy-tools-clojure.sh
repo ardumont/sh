fa6e6b5f2e395cc7795c744c66532678114fb022
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-05-28 16:58:10 +0200

    Upgrade.

--- deploy-tools-clojure.sh
+++ deploy-tools-clojure.sh
@@ -7,8 +7,8 @@ chmod +x ~/bin/lein
 lein -v
 
 # install the plugin to make a server swank
-lein plugin install swank-clojure 1.4.2
-lein plugin install lein-midje 1.0.9
+lein plugin install swank-clojure 1.4.3
+lein plugin install lein-midje 1.0.10
 lein plugin install lein-marginalia 0.7.0
 lein plugin install lein-noir 1.2.1
 lein plugin install lein-cljs 0.2.2
