0b50739ab2c64a7600e8f51ba6e42be9fbf014fd
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-07-08 11:33:36 +0200

    Simplify the yes answer.

--- deploy-java-sun-11.04.sh
+++ deploy-java-sun-11.04.sh
@@ -8,7 +8,7 @@ grep "deb http://archive.canonical.com/ubuntu natty partner" /etc/apt/sources.li
 sudo aptitude update
 
 # Install sun
-yes | sudo aptitude install sun-java6-jdk sun-java6-source
+sudo aptitude install -y sun-java6-jdk sun-java6-source
 
 # Once install choose the sun-jre ad the default one
 sudo update-alternatives --config java
