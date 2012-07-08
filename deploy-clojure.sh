5b7464c6da320c204641a2c211f41bfe65f51a80
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-07-08 11:52:24 +0200

    Simplify clojure installation.

--- deploy-clojure.sh
+++ deploy-clojure.sh
@@ -1,15 +1,7 @@
 #!/bin/bash -x
 
-. $(dirname $0)/standard-functions.sh
+WDIR=$(dirname $0)
 
-CMD="aptitude -y install clojure inotify-tools ruby"
+$WDIR/install.sh "clojure inotify-tools ruby"
 
-if [ -f /etc/lsb-release ]; then #ubuntu
-    # Install clojure
-    sudo $CMD
-elif [ -f /etc/debian_version ]; then # debian
-    su -c "$CMD"
-fi
-
-# Install tools
-$(dirname $0)/deploy-tools-clojure.sh
+$WDIR/deploy-tools-clojure.sh
