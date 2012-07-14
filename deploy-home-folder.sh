9417416d6fcc193481b0a1208dcb616ddfde5df4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-07-14 20:53:52 +0200

    Adapth the script to deploy my home folder.

--- deploy-home-folder.sh
+++ deploy-home-folder.sh
@@ -2,10 +2,16 @@
 
 SRC=$HOME/repositories/perso
 REPO_BIN=$SRC/sh/
+REPO_WORK=$SRC/work/bin
 REPO_TEAM=$SRC/common-sh/
+REPO_DOTFILES=$SRC/dot-files
+
+mkdir -p $SRC
 
 echo ". $(dirname $0)/standard-functions.sh"
 . $(dirname $0)/standard-functions.sh
+
+[ ! -d $REPO_DOTFILES ] && git clone git@github.com/ardumont/dot-files.git $HOME/repositories/perso/
 
 # $HOME/bin
 destroy-link $HOME/bin
@@ -17,7 +23,7 @@ do-action "ln -s $SRC/my-org-files $HOME/org"
 
 # $HOME/.stumpwmrc
 destroy-link $HOME/.stumpwmrc
-do-action "ln -s $SRC/stumpwm-config/.stumpwmrc $HOME/"
+do-action "ln -s $REPO_DOTFILES/.stumpwmrc $HOME/"
 
 # $HOME/.bash*
 destroy-link $HOME/.profile
@@ -29,22 +35,24 @@ destroy-link $HOME/.bash_history
 destroy-link $HOME/.bashrc-prompt
 destroy-link $HOME/.bashrc-work
 
-do-action "ln -s $REPO_BIN/.profile $HOME/"
-do-action "ln -s $REPO_BIN/.bashrc $HOME/"
-do-action "ln -s $REPO_BIN/.bashrc-env $HOME/"
-do-action "ln -s $REPO_BIN/.bashrc-path $HOME/"
-do-action "ln -s $REPO_BIN/.bash_aliases $HOME/"
-do-action "ln -s $REPO_BIN/.bashrc-prompt $HOME/"
-do-action "ln -s $REPO_BIN/.bashrc-work $HOME/"
+do-action "ln -s $REPO_DOTFILES/.profile $HOME/"
+do-action "ln -s $REPO_DOTFILES/.bashrc $HOME/"
+do-action "ln -s $REPO_DOTFILES/.bashrc-env $HOME/"
+do-action "ln -s $REPO_DOTFILES/.bashrc-path $HOME/"
+do-action "ln -s $REPO_DOTFILES/.bash_aliases $HOME/"
+do-action "ln -s $REPO_DOTFILES/.bashrc-prompt $HOME/"
+
+[ -f $REPO_WORK/.bashrc-work ] && do-action "ln -s $REPO_WORK/.bashrc-work $HOME/"
+
 do-action "ln -s $REPO_TEAM ~/team/bin"
 
 # git-bash-completion
 destroy-link $HOME/.git-completion.bash
-do-action "ln -s $HOME/bin/.git-completion.bash $HOME/"
+do-action "ln -s $REPO_DOTFILES/.git-completion.bash $HOME/"
 
 # For conkeror
 destroy-link $HOME/.conkerorrc
-do-action "ln -s $SRC/conkeror-saner-defaults/.conkerorrc $HOME/"
+do-action "ln -s $REPO_DOTFILES/.conkerorrc $HOME/"
 
 # link to some reading
 destroy-link $HOME/lecture
