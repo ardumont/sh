69dc53a2497838f007d9e6af8f67f65b00f54111
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-08-11 19:52:54 +0200

    A simple script to help in commiting rapidly.

---
+++ git-backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+# Use: a simple script to help in backup rapidly some heavy app (eclipse for one example)
+
+COMMIT_MSG=$1
+[ -z "$COMMIT_MSG" ] && COMMIT_MSG="stable"
+
+git add .
+git ls-files -d | xargs git rm
+git commit -am "stable"
