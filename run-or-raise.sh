8b569edb458319ed0eec5aaa9c0300febc65ed1f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-08-11 21:15:33 +0200

    Improve the script to run-or-raise.sh the application.

--- run-or-raise.sh
+++ run-or-raise.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 # emulate stumpwms run-or-raise with ratpoison
-# Param1: Windowname to look for
-# Param2: Command to start if not present yet
+# $1: Windowname to look for
+# $2: Command to start if the Windowname to look for is not found yet, if this is not provided, we use the Windowname
 
 #source: http://lists.gnu.org/archive/html/ratpoison-devel/2010-08/msg00005.html
 
-ratpoison -c windows|grep -q $1
+ratpoison -c windows|grep -iq $1
 
 if [ $? -eq 0 ] ; then
 #    echo "Value grep: $? . selecting"
     ratpoison -c "select $1"
 else
 #    echo "Value grep: $? . starting"
-    $2&
+    if [ -z "$2" ]; then
+        # fallback, we use the same command as the window
+        $1&
+    else
+        $2&
+    fi
 fi
