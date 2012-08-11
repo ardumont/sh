af499e457c2fc94f5fe86b69a711f37727b9d9f9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-08-11 19:56:52 +0200

    Explicit the script's behaviour.

--- git-backup.sh
+++ git-backup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -x
 
 # Use: a simple script to help in backup rapidly some heavy app (eclipse for one example)
+# $0                 add in the git index all the modified and new files, add the deleted ones in the index too then commit with the
+#                    phrase "stable"
+# $0 "some-message"  the same as before but with the commit message 'some-message'
 
 COMMIT_MSG=$1
 [ -z "$COMMIT_MSG" ] && COMMIT_MSG="stable"
