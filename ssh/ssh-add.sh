09d059d668b3d0cfc9108146c1833bcde47b3662
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-08-12 19:16:39 +0200

    Retrieve the code from the past.

---
+++ ssh/ssh-add.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Is the key already loaded into the ssh-agent
+ssh-add -l | grep "~/.ssh/id_rsa"
+if [ $? -ne 0 ]; then
+    # add id_rsa
+    ssh-add
+fi
+
+[ -f ~/work/bin/work-ssh-add.sh ] && ~/work/bin/work-ssh-add.sh
+
