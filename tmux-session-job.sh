957e364c3155e390f49ba852976b7943d8178bd7
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-08-12 15:00:20 +0200

    Added a script to help in presetuping tmux.

---
+++ tmux-session-job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+# Goal: This script targets the templating creation of a tmux session to avoid repeatedly creating the same panes over
+# and over again
+# Use: $0 <name-of-the-session>
+
+SESSION_NAME=$1
+[ -z "$SESSION_NAME" ] && SESSION_NAME=job-session
+
+# create a new session without attaching to it yet with the name $SESSION_NAME
+tmux new-session -d -s $SESSION_NAME
+
+# Then setup some window in it
+tmux new-window -t $SESSION_NAME:1 -n 'Fitnesse'
+tmux new-window -t $SESSION_NAME:2 -n 'Lily'
+tmux new-window -t $SESSION_NAME:3 -n 'hdt'
+tmux new-window -t $SESSION_NAME:4 -n 'wikeo'
+tmux new-window -t $SESSION_NAME:5 -n 'git'
+
+# Select the git window
+tmux select-window -t $SESSION_NAME:5
+# at last, attach to it
+tmux -2 attach-session -t $SESSION_NAME
