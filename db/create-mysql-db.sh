9a8891d170e57fa198cad9497826976944ce0ae1
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-10-08 19:57:21 +0200

    Create a db and grant access to the user.

---
+++ db/create-mysql-db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+# Use: $0 <DBNAME> <USER>
+
+DBNAME=$1
+USER=$2
+
+[ $# != 2 ] && echo "Use: $0 <DBNAME> <USER>" && exit 1
+
+stty -echo
+read -p "Password for '$USER' on the db '$DBNAME':" PWD; echo
+stty echo
+
+mysql -u root -p <<EOF
+CREATE DATABASE $DBNAME;
+GRANT ALL PRIVILEGES ON *.* TO '$USER'@'localhost' IDENTIFIED BY '$PWD' WITH GRANT OPTION;
+EOF
