d70d63245504bc0a73cf61d5bf56698b9831815d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-10-22 08:41:00 +0200

    More detail on the script.

--- ssh/ssh-add.sh
+++ ssh/ssh-add.sh
@@ -1,6 +1,18 @@
 #!/bin/bash -x
 
-for key in $(cat ~/bin/ssh/identity);
+FILE=~/.ssh-agent-identity
+
+if [ ! -f $FILE ] ; then
+    echo "Use: $0"
+    echo "Will load the identity you reference in the file '$FILE': one identity per line"
+    echo ""
+    echo "Example of $FILE's content:"
+    echo "/home/user/.ssh/id_rsa"
+    echo "/home/user/.ssh/id_rsa-for-job"
+    exit 1;
+fi
+
+for key in $(cat $FILE);
 do
     # Is the key already loaded into the ssh-agent?
     ssh-add -L | grep "$key"
