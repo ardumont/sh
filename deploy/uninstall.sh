57b2dc9db0017d6c9b20d1a62b8d7667fcbaa39b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-18 04:42:29 +0100

    A script to interface the removal of package.

---
+++ deploy/uninstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+# This script is destined to simplify the uninstallation between debian and ubuntu
+# Use: uninstall.sh list-of-packages
+# Ex:  uninstall.sh byobu tmux emacs23...
+
+CMD=$*
+[ -z "$CMD" ] && echo "Nothing to install - STOP." && exit 1;
+
+sudo aptitude remove -y $CMD
