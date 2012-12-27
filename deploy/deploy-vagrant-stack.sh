b91d919d86442a64e0b2658ce203ab4d9c0d6aec
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-27 15:02:48 +0100

    Improve the deploy vagrant stack script.

--- deploy/deploy-vagrant-stack.sh
+++ deploy/deploy-vagrant-stack.sh
@@ -4,6 +4,10 @@ WDIR=$(dirname $(readlink -f $0))
 
 $WDIR/install.sh libxslt-dev libxml2-dev libghc-zlib-dev
 
+$WDIR/deploy-ruby.sh
+
+$WDIR/install.sh virtualbox
+
 sudo gem install vagrant vagrant-snap virtualbox
 
 sudo gem install veewee
