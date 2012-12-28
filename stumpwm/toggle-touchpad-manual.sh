b9f0eb18d90df032e4ab20b0c71c81b5f4795dea
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-28 20:43:16 +0100

    Basic brick for toggling the state of the touchpad manually.

--- stumpwm/toggle-touchpad-manual.sh
+++ stumpwm/toggle-touchpad-manual.sh
@@ -1,16 +1,14 @@
 #!/bin/bash -e
 
-# Use: $0
+# Use: $0 <ID> <STATUS>
 # By default the touchpad is activated!
-# $0 on: To activate the touchpad
-# $0 off: To deactivate the touchpad
+# $0 <ID> 1: To activate the touchpad with id <ID>
+# $0 <ID> 0: To deactivate the touchpad with id <ID>
 
-ACTION=${1-:"on"}
+TOUCHPAD_ID=$1
+TOUCHPAD_STATUS=${2-:"1"}
 
-# Find the identifier of the touchpad
-TOUCHPAD_ID=$(xinput list | grep -i touchpad | grep -o "id=[0-9]*" | cut -f 2 -d'=')
-
-if [ $ACTION = "on" ]; then
+if [ $TOUCHPAD_STATUS = "1" ]; then
     # Activate
     xinput --enable $TOUCHPAD_ID
 else
