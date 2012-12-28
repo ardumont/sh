3088e8c67dc468b8182a60dd455dd581b78d749d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-28 20:35:39 +0100

    toggle-touchpad.sh toggle on/off automatically the touchpad depending on its current state

--- stumpwm/toggle-touchpad.sh
+++ stumpwm/toggle-touchpad.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 
 # Use: $0
-# By default the touchpad is activated!
-# $0 on: To activate the touchpad
-# $0 off: To deactivate the touchpad
-
-ACTION=${1-:"on"}
+# Toggle automatically on/off the touchpad depending its state
 
 # Find the identifier of the touchpad
 TOUCHPAD_ID=$(xinput list | grep -i touchpad | grep -o "id=[0-9]*" | cut -f 2 -d'=')
+TOUCHPAD_STATUS=$(xinput --list-props 12 | grep "Device Enabled " | cut -d':' -f 2)
 
-if [ $ACTION = "on" ]; then
+echo "TID: $TOUCHPAD_ID ; TST: $TOUCHPAD_STATUS"
+echo
+
+if [ $TOUCHPAD_STATUS = "0" ]; then
     # Activate
     xinput --enable $TOUCHPAD_ID
 else
