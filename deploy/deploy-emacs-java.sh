a2dd86123c487e5f7075ccc0565b9fe19a9d0618
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-28 19:26:28 +0100

    A script to start setuping emacs to be a java ide because i'm fed up with eclipse et tutti quanti

---
+++ deploy/deploy-emacs-java.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+WDIR=$(dirname $0)
+
+$WDIR/install.sh ecj gcj-jre ecj-gcj
+
+# download jdibug - fully elisp java compiler
+
+cd ~/Downloads
+
+wget http://jdibug.googlecode.com/files/jdibug-0.5.tar.bz2
+
+echo "7b4bec9896295d54bd3c28afac15e1024707fa06" > /tmp/md5sum-check-jdi-reference
+md5sum -c jdibug-05.tar.gz > /tmp/md5sum-check-jdi
+
+diff /tmp/md5sum-check-jdi /tmp/md5sum-check-jdi-reference
