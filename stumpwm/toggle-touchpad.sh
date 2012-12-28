35b1de30af5e44b32c26a5d756ccc8a74029a385
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-28 20:49:52 +0100

    toggle automatic

--- stumpwm/toggle-touchpad.sh
+++ stumpwm/toggle-touchpad.sh
@@ -3,17 +3,12 @@
 # Use: $0
 # Toggle automatically on/off the touchpad depending its state
 
+WDIR=$(readlink -f $(dirname $0))
+
 # Find the identifier of the touchpad
 TOUCHPAD_ID=$(xinput list | grep -i touchpad | grep -o "id=[0-9]*" | cut -f 2 -d'=')
 TOUCHPAD_STATUS=$(xinput --list-props 12 | grep "Device Enabled " | cut -d':' -f 2)
 
-echo "TID: $TOUCHPAD_ID ; TST: $TOUCHPAD_STATUS"
-echo
+[ $TOUCHPAD_STATUS = "0" ] && NEW_STATUS="1" || NEW_STATUS="0"
 
-if [ $TOUCHPAD_STATUS = "0" ]; then
-    # Activate
-    xinput --enable $TOUCHPAD_ID
-else
-    # Deactivate
-    xinput --disable $TOUCHPAD_ID
-fi
+$WDIR/toggle-touchpad-manual.sh $TOUCHPAD_ID $NEW_STATUS
