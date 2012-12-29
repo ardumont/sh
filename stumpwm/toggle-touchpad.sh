eb01500fd42154afdd621b3fa02920257129cf98
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-12-29 13:45:59 +0100

    Fix

--- stumpwm/toggle-touchpad.sh
+++ stumpwm/toggle-touchpad.sh
@@ -7,7 +7,7 @@ WDIR=$(readlink -f $(dirname $0))
 
 # Find the identifier of the touchpad
 TOUCHPAD_ID=$(xinput list | grep -i touchpad | grep -o "id=[0-9]*" | cut -f 2 -d'=')
-TOUCHPAD_STATUS=$(xinput --list-props 12 | grep "Device Enabled " | cut -d':' -f 2)
+TOUCHPAD_STATUS=$(xinput --list-props $TOUCHPAD_ID | grep "Device Enabled " | cut -d':' -f 2)
 
 [ $TOUCHPAD_STATUS = "0" ] && NEW_STATUS="1" || NEW_STATUS="0"
 
