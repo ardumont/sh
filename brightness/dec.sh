68276b2fbf44bcdc7928128057e870aaaff917bb
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-01-04 12:25:18 +0100

    Use an increment (default to 2) to increment or decrement the brightness of the screen.

--- brightness/dec.sh
+++ brightness/dec.sh
@@ -1,2 +1,8 @@
-#!/bin/bash
-pkexec /usr/lib/gnome-settings-daemon/gsd-backlight-helper --set-brightness $(expr $(/usr/lib/gnome-settings-daemon/gsd-backlight-helper --get-brightness) - 1)
+#!/bin/bash -x
+
+# Use: $0 <brightness increment>
+# Decrement the current brightness by the brightness increment passed as parameter
+
+INC=${1:-"2"}
+
+pkexec /usr/lib/gnome-settings-daemon/gsd-backlight-helper --set-brightness $(expr $(/usr/lib/gnome-settings-daemon/gsd-backlight-helper --get-brightness) - $INC)
--- brightness/inc.sh
+++ brightness/inc.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-pkexec /usr/lib/gnome-settings-daemon/gsd-backlight-helper --set-brightness $(expr $(/usr/lib/gnome-settings-daemon/gsd-backlight-helper --get-brightness) + 1)
+
+# Use: $0 <brightness increment>
+# Increment the current brightness by the increment passed as parameter (default 2)
+
+INC=${1:-2}
+
+pkexec /usr/lib/gnome-settings-daemon/gsd-backlight-helper --set-brightness $(expr $(/usr/lib/gnome-settings-daemon/gsd-backlight-helper --get-brightness) + $INC)
