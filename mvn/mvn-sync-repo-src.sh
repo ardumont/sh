0fd5d89f16b8bf045b4e0bad7e195328a1deaf31
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-03-11 09:56:19 +0100

    Adding 2 new scripts to help in fetching sources for every clojure project.

---
+++ mvn/mvn-sync-repo-src.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -xe
+
+# Use
+# $0 /path/to/repo-which-contains-the-project-clj-file
+# Will fetch the sources of the repo
+
+[ -f $1 ] && cd $(dirname $1) || exit 0
+
+lein pom
+mvn dependency:sources
+rm pom.xml
