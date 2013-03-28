70d113459764c14cbec047ea0eccfb286d2c66f2
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-03-28 04:06:00 +0100

    Adding default informations about the script.

--- db/connect-to-mysql-db-local.sh
+++ db/connect-to-mysql-db-local.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
-USER=wikeo
-PASSWD=wikeo
-SERVER=localhost
-DATABASE=wikeocoredb
+USER=$1
+PASSWD=$2
+SERVER=$3
+DATABASE=$4
+
+if [ $# -ne 4 ]; then
+    cat <<EOF
+Use: $0 <USER> <PASSWD> <SERVER> <DB_NAME>
+  - <USER> the database user
+  - <PASSWD> the password for the database user
+  - <SERVER> the server to connect to
+  - <DB_NAME> the database to connect to
+
+  This triggers the connection to the <DB_NAME> on the <SERVER> using the <USER>/<PASSWD> couple.
+
+Example:
+  $0 eleves eleves localhost eleves
+EOF
+
+    exit 1
+fi
 
 echo "mysql -u $USER -p$PASSWD -h $SERVER $DATABASE"
 mysql -u $USER -p$PASSWD -h $SERVER $DATABASE
-
