6d4e47baaa0510dd9912d0bae613d3687c385968
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-09 20:45:28 +0200

    Beginning to prepare the scala env.

---
+++ scala/deploy-scala-env.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+# Use:
+# $0
+
+if [ $# -ne 0 ]; then
+    echo<<EOF
+Use: $0
+This will trigger the installation of the simple-build-tool (sbt).
+EOF
+    exit 1;
+
+fi
+
+# static setup
+
+URL=http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt//0.12.3/sbt.deb
+DDIR=~/Downloads/scala
+DEB=sbt.deb
+DEB_FILE=$DDIR/$DEB
+
+# actions
+
+mkdir -p $DDIR
+
+wget $URL -O $DEB_FILE
+
+sudo dpkg -i $DEB_FILE
