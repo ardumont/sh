3125d0f9c546e0efc39f457b784978b09a43c2df
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-15 18:44:21 +0200

    A script to clone and deploy my emacs-live's fork and my emacs-live-packs repositories

---
+++ deploy/deploy-emacs-live.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+# Use: This script deploy emacs-live and emacs-live-packs from my git repositories
+
+HREPO=$HOME/repo/perso
+
+# First clone the repositories
+[ ! -d $HREPO/emacs-live ] && git clone git@github.com:ardumont/emacs-live.git $HREPO/emacs-live
+[ ! -d $HREPO/emacs-live-packs ] && git clone git@github.com:ardumont/emacs-live-packs.git $HREPO/emacs-live-packs
+
+# Then links to them in $HOME
+ln -nsf $HREPO/emacs-live ~/.emacs.d
+
+# deploy
+$HREPO/emacs-live-packs/deploy.sh
