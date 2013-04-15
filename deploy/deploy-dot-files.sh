846579a545b81f350ac82aec5cb7878956fc63c1
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-15 18:43:26 +0200

    A script to clone and deploy dot-files repository

---
+++ deploy/deploy-dot-files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+# Use: This script deploy emacs-live and emacs-live-packs from my git repositories
+
+HREPO=$HOME/repo/perso
+
+# First clone the repositories
+[ ! -d $HREPO/dot-files ] && git clone git@github.com:ardumont/dot-files.git $HREPO/dot-files
+
+# deploy
+$HREPO/dot-files/deploy.sh
