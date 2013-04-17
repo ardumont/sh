176024767d716cf812080a97801aee4a8a1051ea
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-04-18 00:07:39 +0200

    Factorize the installation code between stumpwm and xmonad.

--- deploy/deploy-stumpwm.sh
+++ deploy/deploy-stumpwm.sh
@@ -1,25 +1,3 @@
 #!/bin/bash
 
-WDIR=$(dirname $0)
-
-PACKAGE=stumpwm
-
-# Install $PACKAGE
-$WDIR/install.sh $PACKAGE
-
-# Create the file
-sudo touch /usr/share/xsessions/$PACKAGE.desktop
-
-# Add the entry for gdm
-(cat <<EOF
-[Desktop Entry]
-        Exec=$PACKAGE
-        TryExec=$PACKAGE
-        Name=$PACKAGE
-        Comment=The Mouseless Window Manager!
-EOF
-)> $PACKAGE.buffer
-
-sudo cp $PACKAGE.buffer /usr/share/xsessions/$PACKAGE.desktop
-
-rm $PACKAGE.buffer
+$(dirname $0)/deploy-wm.sh stumpwm
---
+++ deploy/deploy-wm.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+WDIR=$(dirname $0)
+
+if [ $# -ne 1]; then
+    echo <<EOF
+Use: $0 <WINDOW_MANAGER_NAME>
+- <WINDOW_MANAGER_NAME> name of the window manager to install (stumpwm or xmonad)
+
+This will install the window manager using the linux package manager and installing the file.desktop
+EOF
+fi
+
+PACKAGE=$1
+
+# Install $PACKAGE
+$WDIR/install.sh $PACKAGE
+
+# Create the file
+sudo touch /usr/share/xsessions/$PACKAGE.desktop
+
+# Add the entry for gdm
+(cat <<EOF
+[Desktop Entry]
+        Exec=$PACKAGE
+        TryExec=$PACKAGE
+        Name=$PACKAGE
+        Comment=The Mouseless Window Manager!
+EOF
+)> $PACKAGE.buffer
+
+sudo cp $PACKAGE.buffer /usr/share/xsessions/$PACKAGE.desktop
+
+rm $PACKAGE.buffer
