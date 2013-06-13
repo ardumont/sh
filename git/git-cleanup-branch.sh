e5b1ff95393fa41bbe46f27161ed7321e886f547
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-13 09:25:18 +0200

    Cleanup branch orchestration.

---
+++ git/git-cleanup-branch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Use: $0 <BRANCH_NAME>"
+    echo "  <BRANCH_NAME> Branch to delete locally and remotely from the origin remote"
+    exit 1
+fi
+
+BRANCH_NAME=${1}
+
+echo "Branch to delete: " $BRANCH_NAME
+
+git branch -d $BRANCH_NAME
+
+[ $? != 0 ] && echo "Branch not merged. Really destroy it? (Hit C-c to stop)." && read && git branch -D $BRANCH_NAME
+
+git push origin :$BRANCH_NAME
