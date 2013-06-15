be45b8e832dfcdf5349545329c6da97188f7c59a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-15 12:17:31 +0200

    Separate 2 scripts to deal with the keyboard mapping.

--- kbd/map-altgr-capslock.sh
+++ kbd/map-altgr-capslock.sh
@@ -5,15 +5,16 @@
 #     - capslock by ctrl
 #     - altgr    by alt
 #
-# Test before changing the mapping
 # To have an alt on the right and an easier ctrl on the left.
 # For a much better Emacs experience.
 # ------------------------------------------------------------------------------
 
-if [[ $(xmodmap) == *ISO_Level3_Shift* ]]
-then
-    echo "xmodmap not customized !";
-    map-altgr-capslock.sh
-else
-    echo "xmodmap already customized !";
-fi
+echo "Replacing capsLocks by control ....";
+xmodmap -e 'remove Lock = Caps_Lock'
+xmodmap -e 'keysym Caps_Lock = Control_L'
+xmodmap -e 'add Control = Control_L'
+
+echo "Replacing altgr by leftAlt ....";
+xmodmap -e 'clear mod5'
+xmodmap -e 'add mod1 = Mode_switch'
+xmodmap -e "keysym ISO_Level3_Shift = Alt_L"
