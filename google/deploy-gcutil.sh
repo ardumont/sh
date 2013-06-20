bde82ec856d7897edc22fcefd9f1aa924e676222
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-06-20 11:13:06 +0200

    gcutil

---
+++ google/deploy-gcutil.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -xe
+
+GCUTIL=gcutil-1.8.0.tar.gz
+URL=https://code.google.com/p/google-compute-engine-tools/downloads/detail?name=$GCUTIL
+INSTALL_FOLDER=~/Downloads/google/
+ARCHIVE=${INSTALL_FOLDER}${GCUTIL}
+APP_FOLDER=~/applications/google/
+
+mkdir -p $INSTALL_FOLDER $APP_FOLDER
+wget $URL -O $ARCHIVE
+
+tar xvf $ARCHIVE -C $APP_FOLDER
