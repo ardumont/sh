a40d8c46e9c9cb04c6051ffcc00e16e9d18a9f87
Antoine R. Dumont <ext.adumont@kyriba.com>
2013-09-16 16:07:53 +0200

    Fix adding the --recurse-submodule flag when cloning emacs-live

--- deploy/deploy-emacs-live.sh
+++ deploy/deploy-emacs-live.sh
@@ -6,7 +6,7 @@ HREPO=$HOME/repo/perso
 
 # First clone the repositories
 [ ! -d $HREPO/emacs-live ] && git clone git@github.com:ardumont/emacs-live.git $HREPO/emacs-live
-[ ! -d $HREPO/emacs-live-packs ] && git clone git@github.com:ardumont/emacs-live-packs.git $HREPO/emacs-live-packs
+[ ! -d $HREPO/emacs-live-packs ] && git clone git@github.com:ardumont/emacs-live-packs.git --recurse-submodules $HREPO/emacs-live-packs
 
 # Then links to them in $HOME
 ln -nsf $HREPO/emacs-live ~/.emacs.d
