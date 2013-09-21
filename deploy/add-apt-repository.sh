3ff346fca2515fe9dd89d7602b2a03fa551f9f74
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-09-21 10:13:11 +0200

    Adding a check step to install the needed package before asking the add repository step. diff --git a/bootstrap-install-system.sh b/bootstrap-install-system.sh new file mode 100755 index 0000000..425e27e --- /dev/null +++ b/bootstrap-install-system.sh @@ -0,0 +1,15 @@ +#!/bin/bash -xe + +# This is a bootstrap script to install the needed script +# for installing my overall system. +# To deploy this script, download it directly from github and make it executable. +# wget https://github.com/ardumont/sh/bootstrap-install-system.sh +# chmod +x bootstrap-install-system.sh +# then execute it. + +sudo aptitude install -y git tig emacs24 tmux ruby gitk + +# you must have pre-setup your ssh key to access the github account +CLONE_DIR=$HOME/repo/perso + +[ ! d $CLONE_DIR/sh ] && git clone git@github.com:ardumont/sh.git $CLONE_DIR/sh && ln -s $CLONE_DIR/sh $HOME/bin

--- deploy/add-apt-repository.sh
+++ deploy/add-apt-repository.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -xe
 
+[ $(package-installed.sh software-properties-common) = "NOT" ] && sudo aptitude install -y software-properties-common
+
 sudo add-apt-repository $*
 
 sudo aptitude update
