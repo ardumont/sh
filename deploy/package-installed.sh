7b80ea00bb81e678b1c799cf9108ec3aa62a077b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-09-21 10:12:41 +0200

    Adding a script to check the presence of a package diff --git a/bootstrap-install-system.sh b/bootstrap-install-system.sh new file mode 100755 index 0000000..425e27e --- /dev/null +++ b/bootstrap-install-system.sh @@ -0,0 +1,15 @@ +#!/bin/bash -xe + +# This is a bootstrap script to install the needed script +# for installing my overall system. +# To deploy this script, download it directly from github and make it executable. +# wget https://github.com/ardumont/sh/bootstrap-install-system.sh +# chmod +x bootstrap-install-system.sh +# then execute it. + +sudo aptitude install -y git tig emacs24 tmux ruby gitk + +# you must have pre-setup your ssh key to access the github account +CLONE_DIR=$HOME/repo/perso + +[ ! d $CLONE_DIR/sh ] && git clone git@github.com:ardumont/sh.git $CLONE_DIR/sh && ln -s $CLONE_DIR/sh $HOME/bin

---
+++ deploy/package-installed.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -e
+
+# Script to determine if a package is installed or not
+
+if [ $# -ne 1 ]; then
+    cat <<EOF
+Check if a package is installed or not.
+Returns:
+- DOES-NOT-EXIST if the package provided as argument does not exist.
+- INSTALLED if the package is installed
+- NOT if the package exist but is not installed.
+
+Use: $0 <PACKAGE>
+- PACKAGE Name of the package to check for installation.
+EOF
+    exit 1
+fi
+
+PACKAGE=$1
+
+STATE=$(aptitude show $PACKAGE | grep 'State:' | cut -d':' -f2)
+
+if [ "$STATE" = "" ]; then
+    echo "DOES-NOT-EXIST"
+elif [ "$STATE" = " installed" ]; then
+    echo "INSTALLED"
+else
+    echo "NOT"
+fi
