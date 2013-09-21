29f036e38fd871514f15f6f32ff02797483d2e0d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-09-21 10:13:46 +0200

    Removing some no longer needed steps. diff --git a/bootstrap-install-system.sh b/bootstrap-install-system.sh new file mode 100755 index 0000000..425e27e --- /dev/null +++ b/bootstrap-install-system.sh @@ -0,0 +1,15 @@ +#!/bin/bash -xe + +# This is a bootstrap script to install the needed script +# for installing my overall system. +# To deploy this script, download it directly from github and make it executable. +# wget https://github.com/ardumont/sh/bootstrap-install-system.sh +# chmod +x bootstrap-install-system.sh +# then execute it. + +sudo aptitude install -y git tig emacs24 tmux ruby gitk + +# you must have pre-setup your ssh key to access the github account +CLONE_DIR=$HOME/repo/perso + +[ ! d $CLONE_DIR/sh ] && git clone git@github.com:ardumont/sh.git $CLONE_DIR/sh && ln -s $CLONE_DIR/sh $HOME/bin

--- deploy/deploy-all.sh
+++ deploy/deploy-all.sh
@@ -4,13 +4,13 @@ WDIR=$(dirname $0)
 VERSION=$1
 WITH_JAVA=$2
 
-# Version by default is 12.04
-[ -z $VERSION ] && VERSION=12.04
+# Version by default is 13.04
+[ -z $VERSION ] && VERSION=13.04
 # By default, no java
 [ -z $WITH_JAVA ] && WITH_JAVA=n
 
 # Deploy the basename soft
-$WDIR/deploy-soft.sh $VERSION
+$WDIR/deploy-soft.sh
 
 # Deploy home
 $WDIR/deploy-home-folder.sh
@@ -25,7 +25,7 @@ $WDIR/deploy-emacs24.sh
 $WDIR/deploy-stumpwm.sh
 
 # Deploy conkeror
-$WDIR/deploy-conkeror.sh
+#$WDIR/deploy-conkeror.sh
 
 # Deploy byobu
 #$WDIR/deploy-byobu.sh
@@ -34,7 +34,7 @@ $WDIR/deploy-conkeror.sh
 $WDIR/deploy-clojure.sh
 
 # Install java if asked
-[ $WITH_JAVA = "y" ] && [ -f $WDIR/deploy-java-sun-$VERSION.sh ] && $WDIR/deploy-java-sun-$VERSION.sh
+#[ $WITH_JAVA = "y" ] && [ -f $WDIR/deploy-java-sun-$VERSION.sh ] && $WDIR/deploy-java-sun-$VERSION.sh
 
 # Deploy gnome on 11.10
 [ $VERSION \> "11.10" ] && [ -f $WDIR/deploy-gnome-alternative.sh ] && $WDIR/deploy-gnome-alternative.sh
