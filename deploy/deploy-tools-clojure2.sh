aeb172b72d46ff7bd092ddc797953e93064c1e06
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-09-22 09:28:20 +0200

    Fix

--- deploy/deploy-tools-clojure2.sh
+++ deploy/deploy-tools-clojure2.sh
@@ -3,4 +3,4 @@
 cd ~/bin
 [ ! -f ~/bin/lein ] && wget -O lein2 - https://raw.github.com/technomancy/leiningen/preview/bin/lein && chmod +x ~/bin/lein2 && ln -nfs ~/bin/lein2 ~/bin/lein
 
-lein -v
+~/bin/lein -v
