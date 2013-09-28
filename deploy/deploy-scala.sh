6ff5ecf61778cb5696662d54df69ec98e4091cbb
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-09-28 16:36:36 +0200

    small improvment

--- deploy/deploy-scala.sh
+++ deploy/deploy-scala.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -xe
 
 URL=http://www.scala-lang.org/files/archive/scala-2.10.2.tgz
-DIR=~/applications
-FILE=$HOME/scala.tgz
+DIR=~/applications/scala/
+FILE=$HOME/Downloads/scala.tgz
+
+mkdir -p $DIR
 
 [ ! -f $FILE ] && wget -O $FILE $URL
 
