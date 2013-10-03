5f5af9d2859c95fd24af075cff1c7fca2e4183b1
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-10-03 04:17:10 +0200

    Improve deploy-node.sh script

--- deploy/deploy-node.sh
+++ deploy/deploy-node.sh
@@ -1,11 +1,18 @@
-#!/bin/bash
+#!/bin/bash -xe
 
-wget http://nodejs.org/dist/v0.10.20/node-v0.10.20.tar.gz -O ~/Downloads/node.tgz
+FILE=$HOME/Downloads/node.tgz
+DDIR=$HOME/applications
 
-tar xvf ~/Downloads/node.tgz -C ~/applications/node-v0.10.20
+mkdir -p $DDIR
 
-cd ~/applications/node-v0.10.20
+[ ! -f $FILE ] && wget http://nodejs.org/dist/v0.10.20/node-v0.10.20.tar.gz -O $FILE
 
-./configure
-make
-sudo make install
+if [ ! -d $DDIR/node-v0.10.20 ]; then
+    tar xvf $FILE -C $DDIR
+
+    cd $DDIR/node-v0.10.20
+
+    ./configure
+    make
+    sudo make install
+fi
