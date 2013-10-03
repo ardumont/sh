f2ea209358cf52b970d241cf7325dd7a36b5fe14
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-10-03 04:16:52 +0200

    deploy gradle script

---
+++ deploy/deploy-gradle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+
+URL=http://services.gradle.org/distributions/gradle-1.8-all.zip
+DDIR=$HOME/Downloads
+FILENAME=gradle-1.8-all
+FILE=$DDIR/$FILENAME.zip
+IDIR=$HOME/applications/
+IFINAL_DIR=$IDIR/$FILENAME
+LINKNAME=gradle
+
+[ ! -f $FILE ] && wget -O $FILE $URL
+
+[ ! -d $IFINAL_DIR ] && unzip $FILE -d $IDIR && cd $IDIR && ln -nsf $IFINAL_DIR $LINKNAME
