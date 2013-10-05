4bf1cc691684dfce70ee53cdbc5ade6bc8a713e6
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-10-05 13:48:43 +0200

    adding autojump

--- deploy/deploy-soft.sh
+++ deploy/deploy-soft.sh
@@ -2,4 +2,4 @@
 
 WDIR=$(dirname $0)
 
-$WDIR/install.sh chromium-browser localepurge tree deborphan language-pack-en language-pack-fr gparted stalonetray sweethome3d inotify-tools curl yajl-tools openssh-server sysvbanner most graphviz audacious audacious-plugins gnuplot vim latex latex-beamer texlive-latex-extra texlive-fonts-recommended ttf-marvosym nautilus-dropbox meld dstat htop zip unzip rar unrar p7zip p7zip-rar xclip gpointing-device-settings enca scrot xfce4 xserver-xephyr nethogs multitail tasksel unetbootin acpi
+$WDIR/install.sh chromium-browser localepurge tree deborphan language-pack-en language-pack-fr gparted stalonetray sweethome3d inotify-tools curl yajl-tools openssh-server sysvbanner most graphviz audacious audacious-plugins gnuplot vim latex latex-beamer texlive-latex-extra texlive-fonts-recommended ttf-marvosym nautilus-dropbox meld dstat htop zip unzip rar unrar p7zip p7zip-rar xclip gpointing-device-settings enca scrot xfce4 xserver-xephyr nethogs multitail tasksel unetbootin acpi autojump
