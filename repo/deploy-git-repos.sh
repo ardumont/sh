902b1221f46d437676c6b37b93a03cce5b7fefe4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-10-13 02:00:03 +0200

    simplifying

--- repo/deploy-git-repos.sh
+++ repo/deploy-git-repos.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -xe
 
-[ -z $WDIR ] && WDIR=$HOME/repo/perso
-[ ! -d $WDIR ] && mkdir -p $WDIR
-
-cd $WDIR
-
 for giturl in $(cat ~/bin/repo/git-repos.list); do
-    git clone $giturl $WDIR/
+    git clone --recursive $giturl
 done
