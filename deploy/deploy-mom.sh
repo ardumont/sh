4152f4d4e8c8f6c19c43fabfc4ea6194f0c010be
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-10-16 23:47:18 +0200

    deploy my ovh manager

---
+++ deploy/deploy-mom.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+
+URL=http://mirror.ovh.net/ftp.ovh.net/MoM/MoM-4.1.15.tar.gz
+DDIR=$HOME/Downloads
+FILENAME=MoM-4.1.15
+FILE=$DDIR/$FILENAME.tar.gz
+IDIR=$HOME/applications/
+IFINAL_DIR=$IDIR/$FILENAME
+LINKNAME=mom
+
+[ ! -f $FILE ] && wget -O $FILE $URL
+
+[ ! -d $IFINAL_DIR ] && tar xvf $FILE -C $IDIR && cd $IDIR && ln -nsf $IFINAL_DIR $LINKNAME
