f27246ad843a71b08c6e8b505bf944fde6b04576
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-11-24 22:50:09 +0100

    Remove no longer needed script

--- marmalade/token.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-[ $# -ne 2 ] && echo "token.sh <USER> <PASS>" && exit 1
-
-URL=http://marmalade-repo.org
-USER=$1
-PASS=$2
-
-curl -XPOST "$URL/v1/users/login?name=$USER&password=$PASS"
