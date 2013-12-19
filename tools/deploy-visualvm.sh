97b98ad072cac8ee88446dc9e6d632509bbd61f4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-12-19 11:23:39 +0100

    Deploy visualvm

---
+++ tools/deploy-visualvm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+
+DDIR=$HOME/Downloads
+URL=https://java.net/projects/visualvm/downloads/download/release136/visualvm_136.zip
+FILENAME=visualvm_136
+FILE=$DDIR/$FILENAME.zip
+IDIR=$HOME/applications
+IFINAL_DIR=$IDIR/$FILENAME
+LINKNAME=visualvm
+
+[ ! -f $FILE ] && wget -O $FILE $URL
+
+[ ! -d $IFINAL_DIR ] && unzip $FILE -d $IDIR && cd $IDIR && ln -nsf $IFINAL_DIR $LINKNAME
