dc979ee801a739300e3ffe9d622618463ad0d6d9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-12-19 11:25:46 +0100

    With JMX options java wrapper command

---
+++ java/with-jmx-remote.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -xe
+
+$* -Djava.rmi.server.hostname=$(hostname) -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=6666 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false
