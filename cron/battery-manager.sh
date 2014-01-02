0bea8139f34a93c287213a936f29d81b57a5d987
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-02 22:04:07 +0100

    Upgrade

--- cron/battery-manager.sh
+++ cron/battery-manager.sh
@@ -1,8 +1,10 @@
-#!/bin/sh -x
+#!/bin/sh
+
+# source: https://github.com/massyl/dotFiles/blob/master/batteryManager.sh
 
 battery_charging=`acpi -b | awk '{print $3}'|awk -F"," '{print $1}'`
 battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
 
 export DISPLAY=:0
 
-[ $battery_level -le 20 ] && [ $battery_charging = "Discharging" ] && notify-send "Battery low : ${battery_level}%!"
+[ $battery_level -eq 20  -o $battery_level -le 10 ] && [ $battery_charging = "Discharging" ] && notify-send "Battery low : ${battery_level}%!"
