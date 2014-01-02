a041a9819ef305281f71b5f9aef0a770f92af9bb
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-02 21:46:30 +0100

    Reorganize deploy all softs order

--- deploy/deploy-all.sh
+++ deploy/deploy-all.sh
@@ -2,36 +2,32 @@
 
 WDIR=$(dirname $0)
 VERSION=$1
-WITH_JAVA=$2
+#WITH_JAVA=$2
 
 # Version by default is 13.04
-[ -z $VERSION ] && VERSION=13.04
+#[ -z $VERSION ] && VERSION=13.04
 # By default, no java
-[ -z $WITH_JAVA ] && WITH_JAVA=n
+#[ -z $WITH_JAVA ] && WITH_JAVA=n
 
-# Deploy the basename soft
 $WDIR/deploy-soft.sh
 
-# Deploy home
 $WDIR/deploy-home-folder.sh
 
-# Deploy git config
 $WDIR/deploy-git-config.sh
 
-# Deploy emacs
 $WDIR/deploy-emacs24.sh
 
-# Deploy stumpwm
 $WDIR/deploy-stumpwm.sh
 
-# Deploy conkeror
 #$WDIR/deploy-conkeror.sh
 
-# Deploy byobu
 #$WDIR/deploy-byobu.sh
 
-# Deploy clojure
 $WDIR/deploy-clojure.sh
+
+$WDIR/deploy-haskell.sh
+
+$WDIR/deploy-scala.sh
 
 # Install java if asked
 #[ $WITH_JAVA = "y" ] && [ -f $WDIR/deploy-java-sun-$VERSION.sh ] && $WDIR/deploy-java-sun-$VERSION.sh
