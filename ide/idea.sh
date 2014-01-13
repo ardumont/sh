0fdb366d344ebdbc7807bc413aef5a2587212856
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-13 10:15:02 +0100

    Switch to idea 13

--- ide/idea.sh
+++ ide/idea.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 
-~/applications/ide/idea/idea12/bin/idea.sh
-#~/applications/ide/idea/idea13/bin/idea.sh
+#~/applications/ide/idea/idea12/bin/idea.sh
+~/applications/ide/idea/idea13/bin/idea.sh
