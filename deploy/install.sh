a3f9d82355b61b6e6ad754464371c9ebace39c66
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-19 18:01:13 +0100

    If installing only one package, ensure it is not already installed before launching installation process.

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 # This script is destined to simplify the installation between debian and ubuntu
 # Use: install.sh list-of-packages
@@ -7,9 +7,18 @@
 CMD=$*
 [ -z "$CMD" ] && echo "Nothing to install - STOP." && exit 1;
 
+function package-install() {
+    sudo aptitude install -y $*
+}
+
 # ubuntu
 if [ -f "/etc/lsb-release" ]; then #ubuntu or linux mint
-    sudo aptitude install -y $CMD
+    if [ $# -eq 1 ]; then
+        # one software, we can check if it is already installed or not
+        ( [ ! $(dpkg -l | grep -c "$CMD") -eq 1 ] && echo "Installing package '$CMD'..." && package-install $CMD ) || echo "Package '$CMD' already installed!"
+    else
+        package-install $CMD
+    fi
 elif [ -f "/etc/debian_version" ]; then # debian
     su -c "aptitude install -y $CMD"
 fi
