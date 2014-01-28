e99368fa0f366f1e7566db9e427da01b8b1f55e4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-28 11:29:20 +0100

    Simplify docker install

--- deploy/deploy-docker.sh
+++ deploy/deploy-docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -xe
 
-install.sh linux-image-extra-`uname -r` software-properties-common
+# from http://docs.docker.io/en/latest/installation/ubuntulinux/#ubuntu-raring-saucy
 
-add-apt-repository.sh ppa:dotcloud/lxc-docker
+install.sh linux-image-extra-`uname -r`
 
-install.sh lxc-docker
+curl https://get.docker.io/ubuntu/ | sh
 
 # Now you can run docker
 #docker run -t -t ubuntu /bin/bash
