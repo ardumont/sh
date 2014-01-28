b7cddca829f113a36652e113cec6650f7fa03650
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-28 12:33:04 +0100

    Fix right for installation

--- deploy/deploy-docker.sh
+++ deploy/deploy-docker.sh
@@ -4,7 +4,7 @@
 
 install.sh linux-image-extra-`uname -r`
 
-curl https://get.docker.io/ubuntu/ | sh
+curl https://get.docker.io/ubuntu/ | sudo sh
 
 # Now you can run docker
 #docker run -t -t ubuntu /bin/bash
