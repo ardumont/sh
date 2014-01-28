9bb4c130ae8dcb06c9668f2c08291ff368bdb5ad
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-01-28 11:23:24 +0100

    Use my own script to orchestrate install

--- deploy/deploy-docker.sh
+++ deploy/deploy-docker.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -xe
 
-sudo aptitude install -y linux-image-extra-`uname -r`  software-properties-common
+install.sh linux-image-extra-`uname -r` software-properties-common
 
-sudo add-apt-repository -y ppa:dotcloud/lxc-docker
+add-apt-repository.sh ppa:dotcloud/lxc-docker
 
-sudo aptitude update
-
-sudo aptitude install -y lxc-docker
+install.sh lxc-docker
 
 # Now you can run docker
 #docker run -t -t ubuntu /bin/bash
