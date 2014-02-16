94fd64d7252de28d8472c1579051c359f77c420a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-16 18:36:08 +0100

    Remove duplication from the ~/.Xmodmap file

--- kbd/map-altgr-capslock.sh
+++ kbd/map-altgr-capslock.sh
@@ -1,30 +1,3 @@
 #!/bin/bash
 
-function log() {
-    echo "display '$DISPLAY' - $1"
-}
-
-# ------------------------------------------------------------------------------
-# Replace :
-#     - capslock by ctrl
-#     - altgr    by alt
-#
-# To have an alt on the right and an easier ctrl on the left.
-# For a much better Emacs experience.
-# ------------------------------------------------------------------------------
-
-XMODMAP="xmodmap -display $DISPLAY -e"
-
-# work around pb in xephyr
-$XMODMAP 'keysym Super_L = Num_Lock'
-
-log "Replacing capsLocks by control...";
-$XMODMAP 'remove lock      = Caps_Lock'
-$XMODMAP 'keysym Caps_Lock = Control_L'
-$XMODMAP 'add control      = Control_L'
-
-log "Replacing altgr by leftAlt...";
-$XMODMAP 'clear mod5'
-$XMODMAP 'add mod1                = Mode_switch'
-
-# state 0x10, keycode 36 (keysym 0xff0d, Return), same_screen YES,
+xmodmap -display $DISPLAY ~/.Xmodmap
