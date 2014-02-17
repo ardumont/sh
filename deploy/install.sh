d32ce7eafd5db349472bfbee4eb875036269d693
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-17 10:12:04 +0100

    Simplify the checking for the package installation

--- deploy/install.sh
+++ deploy/install.sh
@@ -15,7 +15,7 @@ function package-install() {
 if [ -f "/etc/lsb-release" ]; then #ubuntu or linux mint
     if [ $# -eq 1 ]; then
         # one software, we can check if it is already installed or not
-        ( [ ! $(dpkg -l | grep -c "$PACKAGES") -eq 1 ] && echo "Installing package '$PACKAGES'..." && package-install $PACKAGES ) || echo "Package '$PACKAGES' already installed!"
+        ( [ $(package-installed.sh $PACKAGES) = "NOT" ] && echo "Installing package '$PACKAGES'..." && package-install $PACKAGES ) || echo "Package '$PACKAGES' already installed or does not exist!"
     else
         package-install $PACKAGES
     fi
