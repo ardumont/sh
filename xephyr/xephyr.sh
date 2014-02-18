4bd4c53af2077d0917debc3b413785bc2911408c
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-18 12:39:38 +0100

    Add options spec

--- xephyr/xephyr.sh
+++ xephyr/xephyr.sh
@@ -9,6 +9,10 @@ HOSTNAME=$(hostname)
 [ $HOSTNAME = "FR-WS-307" -o $HOSTNAME = "dagobah" -o $HOSTNAME = "myrkr" ] && RES="1920x1080" || RES="1366x768"
 
 # Launch a xephyr xserver
+# -ac disable access control
+# -br create root window with black background
+# -noreset don't reset after last client exists
+# -screen XxY specify screen characteristics
 Xephyr -ac -br -noreset -screen $RES $XDP &
 
 # wait a little
