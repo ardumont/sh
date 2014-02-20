edd8da8d5f791f5dad30cfaa772b127673dfb0cb
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-20 16:27:55 +0100

    Update script to check if num lock and caps lock are activated

--- keyboard/caps-lock-notif.sh
+++ keyboard/caps-lock-notif.sh
@@ -3,4 +3,5 @@
 # Send a notification when CAPS LOCK on
 
 # verify if the CAPS is on
-[ "$(led-activated-p.sh)" == "1" ] && notif-boom.sh
+value=$(xset q | grep "Caps Lock" | awk '{print $4}')
+[ "$value" == "on" ] && notif-boom.sh
--- keyboard/num-lock-notif.sh
+++ keyboard/num-lock-notif.sh
@@ -3,4 +3,5 @@
 # Send a notification when NUM LOCK on
 
 # verify if the CAPS is on
-[ $(led-activated-p.sh) == "2" ] && notif-boom.sh
+value=$(xset q | grep "Num Lock" | awk '{print $8}')
+[ "$value" == "on" ] && notif-boom.sh
