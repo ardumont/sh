24e6631d74590aeaefa716a9a8fed0215203615f
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-20 15:10:24 +0100

    Refactor - extract a script to send the notification

--- keyboard/caps-lock-notif.sh
+++ keyboard/caps-lock-notif.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
+# Use: $0
 # Send a notification when CAPS LOCK on
-icon=$HOME/bin/keyboard/kabooom.jpeg
 
 # verify if the CAPS is on
 value=$(xset q | grep "LED mask" | sed -r "s/.*LED mask:\s+[0-9a-fA-F]+([0-9a-fA-F]).*/\1/")
-[ "$value" == "0" ] || notify-send -i $icon "CapsLock ON - BOOOOM" -t 1000
+[ "$value" == "0" ] || notif-boom.sh
---
+++ keyboard/notif-boom.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Use: $0
+# Send a boom notification
+
+icon=$HOME/bin/keyboard/kabooom.jpeg
+
+notify-send -i $icon "CapsLock ON - BOOOOM" -t 1000
