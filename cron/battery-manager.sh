db9ea5d0d2d05ec0b95ec764cdc849425c089f10
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-20 15:21:14 +0100

    Fix punctuation typo

--- cron/battery-manager.sh
+++ cron/battery-manager.sh
@@ -7,4 +7,4 @@ battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
 
 export DISPLAY=:0
 
-[ $battery_level -eq 20  -o $battery_level -le 10 ] && [ $battery_charging = "Discharging" ] && notify-send "Battery low : ${battery_level}%!"
+[ $battery_level -eq 20  -o $battery_level -le 10 ] && [ $battery_charging = "Discharging" ] && notify-send "Battery low: ${battery_level}%!"
