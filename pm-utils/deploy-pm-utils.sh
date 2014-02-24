38414893f21e28707a2bbdcfca27809185143336
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-24 22:24:43 +0100

    Deploy and name it to ensure the mapping is loaded last.

--- pm-utils/deploy-pm-utils.sh
+++ pm-utils/deploy-pm-utils.sh
@@ -3,4 +3,4 @@
 # Script to deploy the pm-utils script to remap the keyboard at wake up time
 #
 
-sudo cp ~/bin/pm-utils/keyboard-mapping-on-resume.sh /etc/pm/sleep.d/00keyboard-mapping
+sudo cp ~/bin/pm-utils/keyboard-mapping-on-resume.sh /etc/pm/sleep.d/0000keyboard-mapping
