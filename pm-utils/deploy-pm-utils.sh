c8a5c48d8da03f52976dcabfc560527d57042e06
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-26 18:40:44 +0100

    Symmetrical script to undeploy personal pm-utils scripts

--- pm-utils/deploy-pm-utils.sh
+++ pm-utils/deploy-pm-utils.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Use: $0
-# Script to deploy the pm-utils script to remap the keyboard at wake up time
+# Script to deploy the personal pm-utils scripts
 #
 
 sudo cp ~/bin/pm-utils/keyboard-mapping-on-resume.sh /etc/pm/sleep.d/0000keyboard-mapping
---
+++ pm-utils/undeploy-pm-utils.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Use: $0
+# Script to remove the personal pm-utils scripts
+#
+
+sudo rm /etc/pm/sleep.d/0000keyboard-mapping
