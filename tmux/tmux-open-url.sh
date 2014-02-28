44e1d59355226f900a387ca4341afab698fab906
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-28 01:08:26 +0100

    Use sensible-browser to abstract the browser used

--- tmux/tmux-open-url.sh
+++ tmux/tmux-open-url.sh
@@ -2,3 +2,4 @@
 # tmux-open-url - open last URL in current tmux pane in firefox
 
 LAST_URL=$(tmux capture-pane \; show-buffer -b0 \; delete-buffer -b0 | grep -P -o '(?:https?://|ftp://|news://|mailto:|file://|\bwww\.)[a-zA-Z0-9\-\@;\/?:&=%\$_.+!*\x27,~#]*(\([a-zA-Z0-9\-\@;\/?:&=%\$_.+!*\x27,~#]*\)|[a-zA-Z0-9\-\@;\/?:&=%\$_+*~])+' | tail -1)
+[ -n "$LAST_URL" ] && sensible-browser "$LAST_URL"
