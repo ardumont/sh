3b8547ff24139949348b9c169cf9ec0240d31aa3
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-28 01:08:10 +0100

    Remove blank lines

--- tmux/tmux-open-url.sh
+++ tmux/tmux-open-url.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 # tmux-open-url - open last URL in current tmux pane in firefox
 
-LAST_URL=$(tmux capture-pane \; show-buffer -b0 \; delete-buffer -b0 |
-           grep -P -o '(?:https?://|ftp://|news://|mailto:|file://|\bwww\.)[a-zA-Z0-9\-\@;\/?:&=%\$_.+!*\x27,~#]*(\([a-zA-Z0-9\-\@;\/?:&=%\$_.+!*\x27,~#]*\)|[a-zA-Z0-9\-\@;\/?:&=%\$_+*~])+' |
-           tail -1)
-[ -n "$LAST_URL" ] && firefox "$LAST_URL"
+LAST_URL=$(tmux capture-pane \; show-buffer -b0 \; delete-buffer -b0 | grep -P -o '(?:https?://|ftp://|news://|mailto:|file://|\bwww\.)[a-zA-Z0-9\-\@;\/?:&=%\$_.+!*\x27,~#]*(\([a-zA-Z0-9\-\@;\/?:&=%\$_.+!*\x27,~#]*\)|[a-zA-Z0-9\-\@;\/?:&=%\$_+*~])+' | tail -1)
