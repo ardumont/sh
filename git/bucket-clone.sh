fd881caf86945dae8b29a9d1515c78e328f716f8
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-01 10:16:53 +0100

    Clone a repo from bitbucket

---
+++ git/bucket-clone.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+REPO=$1
+USER=${2-ardumont}
+
+REMOTE_REPO=git@bitbucket.org:$USER/$REPO.git
+
+([ ! -d $REPO ] && echo "Cloning $REMOTE_REPO into $REPO" && git clone $REMOTE_REPO) || echo "Repo $REPO already cloned, nothing to do."
