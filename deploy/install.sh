06a232177a76f0470cbb1b59c76a98e0da8cbbb0
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-03 21:47:57 +0100

    Use current work dir

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
 # This script is destined to simplify the installation between debian and ubuntu
-# Use: install.sh list-of-packages
-# Ex:  install.sh byobu tmux emacs23...
+# Use: $0 list-of-packages
+# Ex:  $0 byobu tmux emacs23...
 
+WDIR=$(dirname $0)
 PACKAGES=$*
 [ -z "$PACKAGES" ] && echo "Nothing to install - STOP." && exit 1;
 
@@ -15,7 +15,10 @@ function package-install() {
 if [ -f "/etc/lsb-release" ]; then #ubuntu or linux mint
     if [ $# -eq 1 ]; then
         # one software, we can check if it is already installed or not
-        ( [ "$(package-installed.sh $PACKAGES)" = "NOT" ] && echo "Installing package '$PACKAGES'..." && package-install $PACKAGES ) || echo "Package '$PACKAGES' already installed or does not exist!"
+        ( [ "$($WDIR/package-installed.sh $PACKAGES)" = "NOT" ] \
+            && echo "Installing package '$PACKAGES'..." \
+            && package-install $PACKAGES ) \
+              || echo "Package '$PACKAGES' already installed or does not exist!"
     else
         package-install $PACKAGES
     fi
