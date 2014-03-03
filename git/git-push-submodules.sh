d757ce2d0350edab92a791f3280968824387b8b6
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-03 09:29:58 +0100

    Script to orchestrate submodules push

---
+++ git/git-push-submodules.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Use: $0
+# Make the submodule push their current HEAD to the remote branch
+
+WDIR=$1
+BRANCH=master
+ROOT_DIR=$(pwd)
+
+for d in *; do
+    [ -d $d ] && cd $d && git checkout $BRANCH && git push origin $BRANCH
+    cd $ROOT_DIR
+done
