e23645ff9f5677721677352570e3f67aa2df16ab
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-03 21:48:27 +0100

    Force reset

--- git/git-merge-submodules.sh
+++ git/git-merge-submodules.sh
@@ -7,6 +7,6 @@ BRANCH=master
 ROOT_DIR=$(pwd)
 
 for d in *; do
-    [ -d $d ] && cd $d && echo "module: $d" && git checkout $BRANCH && git merge origin/$BRANCH
+    [ -d $d ] && cd $d && echo "module: $d" && git checkout $BRANCH && git reset --hard origin/$BRANCH
     cd $ROOT_DIR
 done
