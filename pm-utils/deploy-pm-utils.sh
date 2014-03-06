7737c6617ec449e518cd968f2f6c0fd97f572a64
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-06 01:05:03 +0100

    Update the hooks to install/uninstall

--- pm-utils/deploy-pm-utils.sh
+++ pm-utils/deploy-pm-utils.sh
@@ -3,4 +3,5 @@
 # Script to deploy the personal pm-utils scripts
 #
 
-sudo cp ~/bin/pm-utils/keyboard-mapping-on-resume.sh /etc/pm/sleep.d/0000keyboard-mapping
+#sudo cp ~/bin/pm-utils/keyboard-mapping-on-resume.sh /etc/pm/sleep.d/0000keyboard-mapping
+sudo cp ~/bin/pm-utils/lock-on-resume.sh /etc/pm/sleep.d/0001session-lock-resume
--- pm-utils/undeploy-pm-utils.sh
+++ pm-utils/undeploy-pm-utils.sh
@@ -3,4 +3,5 @@
 # Script to remove the personal pm-utils scripts
 #
 
-sudo rm /etc/pm/sleep.d/0000keyboard-mapping
+#sudo rm /etc/pm/sleep.d/0000keyboard-mapping
+sudo rm /etc/pm/sleep.d/0001session-lock-resume
