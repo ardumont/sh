6477661cc58be4556c2ac5a1684acb302f6d1976
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-06 01:02:08 +0100

    Update the script with comment and abstract script

--- pm-utils/lock-on-resume.sh
+++ pm-utils/lock-on-resume.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-USER=tony
+# Action script to ensure the session is locked when resuming
+#
+# Copyright: Copyright (c) 2014 Antoine R. Dumont
+# License:   GPL-2
+#
 
+USER=tony
 case $1 in
     suspend|hibernate)
         echo "Nothing to do"
         ;;
     resume|thaw)
-        su -l $USER -c "xscreensaver-command -l" &
+        su -l $USER -c "/home/$USER/bin/session/lock.sh" &
         ;;
 esac
