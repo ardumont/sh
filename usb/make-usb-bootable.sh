fc7738362a8c0d77c3680a2c9f2dc220feb9a76c
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-09 15:04:16 +0100

    Create bootable usb

---
+++ usb/make-usb-bootable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+# Use:
+# make a bootable disk
+
+if [ $# -ne 2 ]; then
+    echo "Use: $0 <ISO> <DEV>"
+    exit 1
+fi
+
+ISO=$1
+DEV=$2
+
+sudo dd if=$1 of=$2 oflag=direct bs=1048576
