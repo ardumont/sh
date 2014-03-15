23b3a22a19a541c5d602267f8793211c9291e0e3
Antoine R. Dumont <eniotna.t@gmail.com>
2014-03-15 11:36:02 +0100

    Add use doc

--- deploy/install-from-source.sh
+++ deploy/install-from-source.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -xe
+# Use: $0
+# Given a folder with source, execute the usual configure, make and use checkinstall for installation of compilation result
 
 ./configure
+
 make
+
 sudo checkinstall
