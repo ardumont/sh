8a34dcfe11997544b3d43828956aaec80582ea4c
Antoine R. Dumont <eniotna.t@gmail.com>
2014-03-16 08:41:10 +0100

    A script to help deploying jekyll locally to preview the main page from a github repository

---
+++ github/deploy-jekyll.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# source: https://help.github.com/articles/using-jekyll-with-pages
+
+# pre-requisite
+install.sh ruby1.9.1-dev
+
+# install bundler
+sudo gem install bundler
+
+# 'install' jekyll in the current git repo
+echo -e "source 'https://rubygems.org'\ngem 'github-pages'" > Gemfile
+
+# execute the bundle installation of the deps needed
+bundle install
