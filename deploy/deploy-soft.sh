c4bc1f5ef825363f59905c709dc666b1e7964c02
Antoine R. Dumont <eniotna.t@gmail.com>
2014-04-05 14:21:09 +0200

    Move nautilus-dropbox to default

--- deploy/deploy-soft.sh
+++ deploy/deploy-soft.sh
@@ -4,7 +4,7 @@ WDIR=$(dirname $0)
 
 ALL=$1
 
-$WDIR/install.sh git gitk tig meld chromium localepurge tree deborphan lsb-languages gparted stalonetray inotify-tools curl yajl-tools openssh-server sysvbanner most graphviz gnuplot vim latex latex-beamer texlive-latex-extra texlive-fonts-recommended ttf-marvosym dstat htop zip unzip rar unrar p7zip p7zip-rar xclip gpointing-device-settings enca scrot xserver-xephyr nethogs multitail tasksel unetbootin acpi autojump mc exuberant-ctags augeas-tools fortune zsh checkinstall apt-file sbcl autoconf texinfo rlwrap fbreader libnss-myhostname xterm xautolock mrxvt xscreensaver keychain ssh-askpass-fullscreen xosview gnome-power-manager
+$WDIR/install.sh git gitk tig meld chromium localepurge tree deborphan lsb-languages gparted stalonetray inotify-tools curl yajl-tools openssh-server sysvbanner most graphviz gnuplot vim latex latex-beamer texlive-latex-extra texlive-fonts-recommended ttf-marvosym dstat htop zip unzip rar unrar p7zip p7zip-rar xclip gpointing-device-settings enca scrot xserver-xephyr nethogs multitail tasksel unetbootin acpi autojump mc exuberant-ctags augeas-tools fortune zsh checkinstall apt-file sbcl autoconf texinfo rlwrap fbreader libnss-myhostname xterm xautolock mrxvt xscreensaver keychain ssh-askpass-fullscreen xosview gnome-power-manager nautilus-dropbox
 
 # optional
-[ ! -z "$ALL" ] && $WDIR/install.sh sweethome3d audacious audacious-plugins nautilus-dropbox xfce4 gnome-power-manager
+[ ! -z "$ALL" ] && $WDIR/install.sh sweethome3d audacious audacious-plugins xfce4 gnome-power-manager
