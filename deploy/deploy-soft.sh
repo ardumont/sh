3b2d6d02a9e1a42283c9500106890ec61ad6122c
Antoine R. Dumont <eniotna.t@gmail.com>
2014-04-05 14:19:07 +0200

    Rework the deploy-soft.sh

--- deploy/deploy-soft.sh
+++ deploy/deploy-soft.sh
@@ -2,4 +2,9 @@
 
 WDIR=$(dirname $0)
 
-$WDIR/install.sh git gitk tig meld chromium localepurge tree deborphan lsb-languages gparted stalonetray sweethome3d inotify-tools curl yajl-tools openssh-server sysvbanner most graphviz audacious audacious-plugins gnuplot vim latex latex-beamer texlive-latex-extra texlive-fonts-recommended ttf-marvosym nautilus-dropbox dstat htop zip unzip rar unrar p7zip p7zip-rar xclip gpointing-device-settings enca scrot xfce4 xserver-xephyr nethogs multitail tasksel unetbootin acpi autojump mc exuberant-ctags augeas-tools fortune zsh checkinstall apt-file sbcl autoconf texinfo rlwrap fbreader libnss-myhostname xterm xautolock mrxvt xscreensaver keychain ssh-askpass-fullscreen xosview gnome-power-manager
+ALL=$1
+
+$WDIR/install.sh git gitk tig meld chromium localepurge tree deborphan lsb-languages gparted stalonetray inotify-tools curl yajl-tools openssh-server sysvbanner most graphviz gnuplot vim latex latex-beamer texlive-latex-extra texlive-fonts-recommended ttf-marvosym dstat htop zip unzip rar unrar p7zip p7zip-rar xclip gpointing-device-settings enca scrot xserver-xephyr nethogs multitail tasksel unetbootin acpi autojump mc exuberant-ctags augeas-tools fortune zsh checkinstall apt-file sbcl autoconf texinfo rlwrap fbreader libnss-myhostname xterm xautolock mrxvt xscreensaver keychain ssh-askpass-fullscreen xosview gnome-power-manager
+
+# optional
+[ ! -z "$ALL" ] && $WDIR/install.sh sweethome3d audacious audacious-plugins nautilus-dropbox xfce4 gnome-power-manager
