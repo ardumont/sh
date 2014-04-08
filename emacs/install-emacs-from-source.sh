a97630326a4ea474452e9792bf338bd2a2fa9467
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-04-08 13:19:37 +0200

    Clone emacs' git folder repository if not present on machine

--- emacs/install-emacs-from-source.sh
+++ emacs/install-emacs-from-source.sh
@@ -2,6 +2,12 @@
 
 install.sh textinfo build-essential automake autoconf libdbus-1-dev libgconf2-dev libgif-dev libgpm-dev libgtk2.0-dev libjpeg62-dev libm17n-dev libncurses5-dev libotf-dev librsvg2-dev libtiff4-dev libXpm-dev
 
+EMACS_SOURCE_FOLDER=$HOME/repo/perso/emacs
+
+[ ! -d $EMACS_SOURCE_FOLDER ] && git clone git://git.savannah.gnu.org/emacs.git $EMACS_SOURCE_FOLDER
+
+cd $EMACS_SOURCE_FOLDER
+
 ./configure --with-dir=$HOME/applications/emacs-snapshot
 make
 src/emacs --version
