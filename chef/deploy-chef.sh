f9d48e078fde7994996a5456423dfc299b50e278
Antoine R. Dumont <eniotna.t@gmail.com>
2014-04-15 19:28:10 +0200

    Add deploy chef script

---
+++ chef/deploy-chef.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# chef client
+# curl -L https://www.opscode.com/chef/install.sh | sudo bash
+
+# chef server
+# wget https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef-server_11.0.12-1.ubuntu.12.04_amd64.deb
+
+# It's in the package
+install.sh chef
