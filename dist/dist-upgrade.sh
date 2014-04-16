f7508127272ef5d7fe4ad963d44228f8bc0ac0e7
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-04-16 19:31:09 +0200

    Add naive dist-upgrade.sh script to migrate mint distribution

---
+++ dist/dist-upgrade.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo sed -i 's/raring/saucy/' /etc/apt/sources.list
+sudo sed -i 's/olivia/petra/' /etc/apt/sources.list
+sudo sed -i 's/raring/saucy/' /etc/apt/sources.list.d/official-package-repositories.list
+sudo sed -i 's/olivia/petra/' /etc/apt/sources.list.d/official-package-repositories.list
+
+sudo apt-get update && sudo apt-get dist-upgrade
+sudo apt-get upgrade
