3b362a232596744e5d340ffec1ed931f606f899a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-04-18 12:43:52 +0200

    Update again the open and forward routine port (still not working)

--- vbox/open-and-forward-port.sh
+++ vbox/open-and-forward-port.sh
@@ -8,15 +8,22 @@ GUEST_NAME=$1
 RULE_NAME=$2
 GUEST_PORT=$3
 HOST_PORT=$4
-PROTOCOL=tcp
+
+PROTOCOL=TCP
+# use logs in virtualbox gui at the moment (not found anything better yet)
+DEVICE=e1000
 
 # Pause the vm to be able to modify it
 VBoxManage controlvm $GUEST_NAME pause
 
-# Remove if the rules exist
-VBoxManage modifyvm $GUEST_NAME --natpf1 delete "$RULE_NAME"
-# Than add the current rule
-VBoxManage modifyvm $GUEST_NAME --natpf1 "$RULE_NAME,$PROTOCOL,,$HOST_PORT,,$GUEST_NAME"
+# # Remove if the rules exist
+# VBoxManage modifyvm $GUEST_NAME --natpf1 delete "$RULE_NAME"
+# # Than add the current rule
+# VBoxManage modifyvm $GUEST_NAME --natpf1 "$RULE_NAME,$PROTOCOL,,$HOST_PORT,,$GUEST_NAME"
+
+VBoxManage setextradata "$GUEST_NAME" "VBoxInternal/Devices/$DEVICE/0/LUN#0/Config/$RULE_NAME/HostPort" $HOST_PORT
+VBoxManage setextradata "$GUEST_NAME" "VBoxInternal/Devices/$DEVICE/0/LUN#0/Config/$RULE_NAME/GuestPort" $GUEST_PORT
+VBoxManage setextradata "$GUEST_NAME" "VBoxInternal/Devices/$DEVICE/0/LUN#0/Config/$RULE_NAME/Protocol" $PROTOCOL
 
 # Pause the vm to be able to modify it
 VBoxManage controlvm $GUEST_NAME resume
