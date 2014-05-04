8ccc594b0d55278548bc09d4295203d7a650cdd8
Antoine R. Dumont <eniotna.t@gmail.com>
2014-05-04 18:27:56 +0200

    Open the means to read a flat .authinfo (less secure)

--- offlineimap/offlineimap-helpers.py
+++ offlineimap/offlineimap-helpers.py
@@ -6,3 +6,9 @@ def get_password_emacs(machine, login, port):
     p = re.compile(s)
     authinfo = os.popen("gpg --quiet --no-tty --decrypt ~/.authinfo.gpg").read()
     return p.search(authinfo).group(1)
+
+def get_plain_password_emacs(machine, login, port):
+    s = "machine %s login %s port %s password ([^ ]*)\n" % (machine, login, port)
+    p = re.compile(s)
+    authinfo = os.popen("cat ~/.authinfo").read()
+    return p.search(authinfo).group(1)
