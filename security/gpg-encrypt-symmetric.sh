e1f600604d5feaf295b916e1027299cef69fff1b
Antoine R. Dumont <eniotna.t@gmail.com>
2014-05-04 18:40:14 +0200

    Add a script to gpg encrypt with passphrase using aes256

---
+++ security/gpg-encrypt-symmetric.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+FILE=$1
+OUTPUT_FILE="$FILE.gpg"
+
+[ $# -ne 1 ] && echo "Use: $0 <FILE-TO-SYMMETRICALLY-ENCRYPT>" && exit 1
+
+gpg -o $OUTPUT_FILE --cipher-algo AES256 --symmetric $FILE
