d1c3ee1d58ba41b65cfd08b3a3c7467f2730ef0f
Antoine R. Dumont <eniotna.t@gmail.com>
2014-05-05 20:03:27 +0200

    Refactor - Replace deprecated code with subprocess calls

--- offlineimap/offlineimap-helpers.py
+++ offlineimap/offlineimap-helpers.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
-import re, os
+import re, subprocess
+
+def read_cmd_from_shell(command):
+    return subprocess.Popen(command, shell=True, bufsize=1024, stdout=subprocess.PIPE).stdout.read()
 
 def get_password_emacs(machine, login, port):
     s = "machine %s login %s port %s password ([^ ]*)\n" % (machine, login, port)
     p = re.compile(s)
-    authinfo = os.popen("gpg --quiet --no-tty --decrypt ~/.authinfo.gpg").read()
+    authinfo = read_cmd_from_shell("gpg --quiet --no-tty --decrypt ~/.authinfo.gpg")
     return p.search(authinfo).group(1)
 
 def get_plain_password_emacs(machine, login, port):
     s = "machine %s login %s port %s password ([^ ]*)\n" % (machine, login, port)
     p = re.compile(s)
-    authinfo = os.popen("cat ~/.authinfo").read()
+    authinfo = read_cmd_from_shell("cat ~/.authinfo")
     return p.search(authinfo).group(1)
