e799c2074e150b92de4cc78ba6a57ce7b41ee8c5
Antoine R. Dumont <eniotna.t@gmail.com>
2014-05-06 19:47:40 +0200

    Refactor - Update retriever function name

--- offlineimap/offlineimap-helpers.py
+++ offlineimap/offlineimap-helpers.py
@@ -4,13 +4,13 @@ import re, subprocess
 def read_cmd_from_shell(command):
     return subprocess.Popen(command, shell=True, bufsize=1024, stdout=subprocess.PIPE).stdout.read()
 
-def get_password_emacs(filename, machine, login, port):
+def get_secure_credentials(filename, machine, login, port):
     s = "machine %s login %s port %s password ([^ ]*)\n" % (machine, login, port)
     p = re.compile(s)
-    authinfo = read_cmd_from_shell("gpg --quiet --no-tty --decrypt %s " % filename)
+    authinfo = read_cmd_from_shell("gpg2 --quiet --no-tty --decrypt %s " % filename)
     return p.search(authinfo).group(1)
 
-def get_plain_password_emacs(filename, machine, login, port):
+def get_plain_credentials(filename, machine, login, port):
     s = "machine %s login %s port %s password ([^ ]*)\n" % (machine, login, port)
     p = re.compile(s)
     authinfo = read_cmd_from_shell("cat %s" % filename)
