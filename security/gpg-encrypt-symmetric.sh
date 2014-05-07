d01e07d952ce14da7fa6b68c1123020c16dede5a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-07 12:55:33 +0200

    Use gpg2

--- security/gpg-encrypt-symmetric.sh
+++ security/gpg-encrypt-symmetric.sh
@@ -5,4 +5,4 @@ OUTPUT_FILE="$FILE.gpg"
 
 [ $# -ne 1 ] && echo "Use: $0 <FILE-TO-SYMMETRICALLY-ENCRYPT>" && exit 1
 
-gpg -o $OUTPUT_FILE --cipher-algo AES256 --symmetric $FILE
+gpg2 -o $OUTPUT_FILE --cipher-algo AES256 --symmetric $FILE
