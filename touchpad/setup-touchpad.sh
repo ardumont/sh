083cf073a6287749c60593bd7645ad98a5eee9ae
Antoine R. Dumont <eniotna.t@gmail.com>
2014-05-09 12:42:27 +0200

    Add a script to persist my touchpad's preference

---
+++ touchpad/setup-touchpad.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+
+# disable palm detection
+synclient PalmDetect=1
+
+# Activate tap to click
+synclient TapButton1=1
+
+# Activate secondary tap to click (to display contextual menu)
+synclient TapButton3=1
+
+# disable the touchpad (for information)
+#synclient TouchpadOff=1
