a394a005d4b9b14ad65afc06d1ca8f9695dda6e9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-05-14 11:38:19 +0200

    Add a script to ease the connection through sqlplus

---
+++ oracle/connect-to-oracle-db.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+
+# Pre-requisite:
+# - install sqlplus and update PATH with sqlplus installation folder
+# - update LD_LIBRARY_PATH with shared library folder needed by sqlplus
+[ $# -ne 4 ] && echo "Use: $0 <USERNAME> <HOST> <PORT> <SSID>" && exit 1
+
+USERNAME=$1
+HOST=$2
+PORT=$3
+SSID=$4
+
+sqlplus $USERNAME@"(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$HOST)(PORT=$PORT))(CONNECT_DATA=(SID=$SID)))"
