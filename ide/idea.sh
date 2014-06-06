27734108d6fe2834a3005a54232eee400c6c4117
Antoine R. Dumont <eniotna.t@gmail.com>
2014-06-06 14:57:32 +0200

    Adapt JAVA_HOME before launching idea (need openjdk to avoid refresh problem in xmonad)

--- ide/idea.sh
+++ ide/idea.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -xe
 
+# export JAVA_HOME=/usr/lib/jvm/jdk1.7.0/
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+
 #~/applications/ide/idea/idea12/bin/idea.sh
 ~/applications/ide/idea/idea13/bin/idea.sh
