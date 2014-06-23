c5e9fa215d1376681000a6f7638f83320aa3daec
Antoine R. Dumont <eniotna.t@gmail.com>
2014-06-23 12:15:37 +0200

    Fix - remove useless command call

--- ide/eclipse.sh
+++ ide/eclipse.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-[ -f ~/workspace/projet-wikeo/.metadata/.lock ] && rm ~/workspace/projet-wikeo/.metadata/.lock
-
 HOST=$(hostname)
 
 [ "$HOST" = "job" ] && ECLIPSE=eclipse || ECLIPSE=~/applications/eclipse/eclipse
