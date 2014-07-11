89fef6b9612f0298d8e24dcb74dab12ecfb8584b
Antoine R. Dumont <eniotna.t@gmail.com>
2014-07-11 15:45:18 +0200

    Update on edbi install

--- oracle/deploy-edbi.sh
+++ oracle/deploy-edbi.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+#Use; Run as root
 
 # to use with edbi https://github.com/kiwanami/emacs-edbi
 
-sudo cpan RPC::EPC::Service
-sudo cpan DBD::Oracle
+export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib:$LD_LIBRARY_PATH
+export ORACLE_HOME=/usr/lib/oracle/12.1/client64
+export PATH=$ORACLE_HOME/bin:$PATH
+
+cpan RPC::EPC::Service
+cpan DBD::Oracle
