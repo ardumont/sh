163bbc6b50728860bc51999357c674d68a56447a
Antoine R. Dumont <eniotna.t@gmail.com>
2014-07-21 12:13:02 +0200

    Force a JAVA_HOME if one is not set already

--- ide/idea.sh
+++ ide/idea.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# export JAVA_HOME=/usr/lib/jvm/jdk1.7.0/
-export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+[ -z $JAVA_HOME ] && export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 
 #~/applications/ide/idea/idea12/bin/idea.sh
 ~/applications/ide/idea/idea13/bin/idea.sh
