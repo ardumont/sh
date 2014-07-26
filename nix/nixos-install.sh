9e8769e2681ebe8d26901545af2c5bce1b2d1b5b
Antoine R. Dumont <eniotna.t@gmail.com>
2014-07-26 17:18:12 +0200

    NixOS install - Add wget download step as pre-requisite

--- nix/nixos-install.sh
+++ nix/nixos-install.sh
@@ -8,6 +8,8 @@
 # wpa_passphrase SSID PASSPHRASE > /etc/wpa_supplicant.conf
 # systemctl restart wpa_supplicant.services
 
+# nix-env -i wget
+
 # partition disk:
 # fdisk /dev/sda
 # sda1 /boot
@@ -26,7 +28,6 @@ nixos-generate-config --root=/mnt
 
 [ -f /etc/wpa_supplicant.conf ] && cp /etc/wpa_supplicant.conf /mnt/etc/
 
-nix-env -i wget
 wget https://raw.githubusercontent.com/ardumont/dot-files/master/nixos/configuration.nix --output-document /mnt/etc/nixos/configuration.nix
 
 nixos-install
