37056a92599c8385238bdd04f807403626c91d11
Antoine R. Dumont <eniotna.t@gmail.com>
2014-11-02 14:41:59 +0100

    Reference some work on how to install tuxguitar (unfinished)

--- guitar/deploy-tuxguitar.sh
+++ guitar/deploy-tuxguitar.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-install.sh tuxguitar tuxguitar-jsa
+wget http://downloads.sourceforge.net/project/tuxguitar/TuxGuitar/TuxGuitar-1.2/tuxguitar-1.2-linux-x86.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Ftuxguitar%2F&ts=1412497814&use_mirror=freefr
+
+#install.sh tuxguitar tuxguitar-jsa
