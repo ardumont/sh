be994f8a6bb04f560c57002a3086c014c91c670a
Antoine R. Dumont <eniotna.t@gmail.com>
2014-11-09 12:28:10 +0100

    Fix violent exit into a sweet return

--- touchpad/disable-touchpad-when-typing.sh
+++ touchpad/disable-touchpad-when-typing.sh
@@ -8,7 +8,7 @@
 # -t:   Only disable tapping and scrolling, not mouse movements, in response to keyboard activity.
 
 # Checkk if syndaemon present, if not, we exit immediately
-which syndaemon 2>&1 >/dev/null || exit 0
+which syndaemon 2>&1 >/dev/null || return 0
 
 SYN_PID=$(pgrep syndaemon)
 
