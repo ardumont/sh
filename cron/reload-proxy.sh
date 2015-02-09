ab35aafb41289b007098f220afe7ada89f64a3e8
Antoine R. Dumont <eniotna.t@gmail.com>
2015-02-09 12:50:10 +0100

    Add cron action
    To install:
    
    ```sh
    crontab - < ~/bin/cron/cron.txt
    ```

---
+++ cron/reload-proxy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# hack script to reload the proxy
+# provided the right env variables are set this does the trick...
+# fixme: understand this issue, search and fix the trouble at source
+
+curl https://duckduckgo.com 1> /dev/null
