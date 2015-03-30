52261747831a84bd5608842007a9d29db6ee0c99
Antoine R. Dumont <eniotna.t@gmail.com>
2015-03-30 10:55:12 +0200

    Modularize code

--- utils/files.py
+++ utils/files.py
@@ -1,32 +1,10 @@
-#!/usr/bin/env python
-
 import os,string,glob
 import argparse
 import functools
 
-######### parser #########
-
-# parse the arguments/options of the script
-parser = argparse.ArgumentParser(description='Process (recursively) files from a target folder to rename folder and files with convention (lower case and - separated).')
-# optional
-parser.add_argument('-t', '--target'            , dest='target_folder_or_file'      , nargs='?', default=".", help='Target file or folder to rename. Default to current folder.')
-parser.add_argument('-f', '--filter'            , dest='filter_files_pattern'       , nargs='?', default='*', help='Filter pattern on files to apply. Default to all files.')
-parser.add_argument('-u', '--user-file-patterns', dest='user_file_specific_patterns', nargs='?', default='' , help='The user\'s file containing the specific patterns to remove when renaming filenames.')
-# flags
-parser.add_argument('-v', '--verbose'  , dest='verbose'  , default=False, action='store_const', const='True', help='Verbose output (default: false)')
-parser.add_argument('-r', '--recursive', dest='recursive', default=False, action='store_const', const='True', help='Recursive (default: false)')
-parser.add_argument('-n', '--dry-run',   dest='dryrun'   , default=False, action='store_const', const='True', help='Dry run (default: false)')
-
-args = parser.parse_args()
-
-######### functions #########
-
 def load_user_specific_patterns_from(filename):
     """Load user's filters from a specific file. Return ths user's filters if existing, empty list otherwise."""
-    if os.path.isfile(filename):
-        return filter(lambda e: e != "", open(filename).read().split("\n"))
-    else:
-        return [];
+    return filter(lambda e: e != "", open(filename).read().split("\n")) if os.path.isfile(filename) else []
 
 def files_from(directory, filter_files_pattern):
     """List the files from the directory using the filter filter_files_pattern."""
@@ -35,15 +13,17 @@ def files_from(directory, filter_files_pattern):
 
 def rename_according_to_naming_conventions(s, user_specific_patterns):
     """Rename according to conventions (no ',', ';', ':', ' ' or '_' replaced by '-')."""
-    s0 = modify_name(s.lower(), user_specific_patterns, '')
-    s1 = modify_name(s0, ('          ', '        ', '       ', '     ', '    ', '   ', '  '), ' ')
-    s2 = modify_name(s1, (' \'', '_\'', '-\''), '\'')
-    s3 = modify_name(s2, (';', ':', '?', '!'), '')
-    s4 = modify_name(s3, ('()', '( )', '(  )', '{}', '{ }', '{  }', '[]', '[ ]', '[  ]'), '')
-    s5 = modify_name(s4, ('..........', '.........', '........', '.......', '......', '.....', '....', '...', '..'), '.')
-    return modify_name(s5, (' _ ', ' - ', ' ', '_',
-                          '__________', '_________', '________', '_______', '_____', '____', '___', '__',
-                          '----------', '---------', '--------', '-------', '-----', '----', '---', '--'), '-')
+    # 0 -> patterns source; 1 -> symbol that replace the patterns source
+    patterns = ( (user_specific_patterns, '')
+                 ,(('          ', '        ', '       ', '     ', '    ', '   ', '  '), ' ')
+                 ,((' \'', '_\'', '-\''), '\'')
+                 ,((';', ':', '?', '!', ','), '')
+                 ,(('()', '( )', '(  )', '{}', '{ }', '{  }', '[]', '[ ]', '[  ]'), '')
+                 ,(('..........', '.........', '........', '.......', '......', '.....', '....', '...', '..'), '.')
+                 ,((' _ ', ' - ', ' ', '_',
+                    '__________', '_________', '________', '_______', '_____', '____', '___', '__',
+                    '----------', '---------', '--------', '-------', '-----', '----', '---', '--'), '-'))
+    return functools.reduce(lambda result, pattern: modify_name(result, pattern[0], pattern[1]), patterns, s.lower())
 
 def modify_name(s, src_replace, dest_replace):
     """Modify the filename : replace all the spaces by underscores and convert from upper to lower case."""
@@ -100,10 +80,3 @@ def scan_and_rename(file_or_folder, filter_files_pattern, user_file_specific_pat
             rename_foldername(file_or_folder, user_specific_patterns, dryrun, verbose)
         else:
             rename_filename(file_or_folder, user_specific_patterns, dryrun, verbose)
-
-scan_and_rename(args.target_folder_or_file
-              , args.filter_files_pattern
-              , args.user_file_specific_patterns
-              , args.recursive
-              , args.dryrun
-              , args.verbose)
