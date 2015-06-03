a6bf296a197d9ae85c8292274628ebcd5aab5aa5
Antoine R. Dumont <eniotna.t@gmail.com>
2015-06-03 14:10:59 +0200

    Simplify this mess

--- ide/idea.sh
+++ ide/idea.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-[ -z $JAVA_HOME ] && export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+#[ -z $JAVA_HOME ] && export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 
 #~/applications/ide/idea/idea12/bin/idea.sh
-~/applications/ide/idea/idea13/bin/idea.sh
+#~/applications/ide/idea/idea13/bin/idea.sh
+idea-community
