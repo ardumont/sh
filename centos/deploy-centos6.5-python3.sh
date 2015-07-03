778db0f04b2942b8807f857705050aa77a68c5e9
Antoine R. Dumont <eniotna.t@gmail.com>
2015-07-03 10:56:21 +0200

    Update actions to undertake to have python3 and pip3 on centos6.5

---
+++ centos/deploy-centos6.5-python3.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# sudo yum groupinstall "Development tools"
+# sudo yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel
+# wget https://www.python.org/ftp/python/3.4.3/Python-3.4.3.tgz -O /var/tmp/Python-3.4.3.tar.gz
+
+# tar xvf /var/tmp/Python-3.4.3.tar.gz -C /var/tmp
+
+# cd /var/tmp/Python-3.4.3
+# make
+# sudo make altinstall
+
+# install the right epel
+sudo yum install -y https://dl.iuscommunity.org/pub/ius/stable/CentOS/6/x86_64/ius-release-1.0-14.ius.centos6.noarch.rpm
+sudo yum install -y python34u python34u.pip
