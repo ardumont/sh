d0d2e18b2b7e6fee6eaa49928be512c18bc13b95
Antoine R. Dumont <eniotna.t@gmail.com>
2015-08-18 09:08:55 +0100

    Update runtime deps for evm
    evm compiles emacs...

--- emacs/deploy-evm.sh
+++ emacs/deploy-evm.sh
@@ -3,6 +3,9 @@
 EVM_FOLDER=/usr/local/evm
 EVM_HOME=$HOME/.evm
 
+# runtime deps when using evm
+sudo apt-get install -y texinfo build-essential automake autoconf libdbus-1-dev libgconf2-dev libgif-dev libgpm-dev libgtk2.0-dev libjpeg-dev libm17n-dev libncurses5-dev libotf-dev librsvg2-dev libtiff5-dev libXpm-dev
+
 [ ! -d $EVM_FOLDER ] && sudo mkdir $EVM_FOLDER && sudo chown $USER: $EVM_FOLDER
 
 [ ! -d $EVM_HOME ] && (curl -fsSkL https://raw.github.com/rejeep/evm/master/go | bash)
