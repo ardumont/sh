9cecfb002e584837f188e7badc73f14b3beeafdc
Antoine R. Dumont <eniotna.t@gmail.com>
2015-10-18 12:54:06 +0200

    Refactor - More pythonic code

--- utils/files.py
+++ utils/files.py
@@ -1,20 +1,50 @@
-import os,string,glob
-import argparse
+import os
+import glob
 import functools
 
-def load_user_specific_patterns_from(filename):
-    """Load user's filters from a specific file. Return ths user's filters if existing, empty list otherwise."""
-    return filter(lambda e: e != "", open(filename).read().split("\n")) if os.path.isfile(filename) else []
+
+def load_user_specific_patterns_from(filepath):
+    """Load user's filters from a specific file.
+    Args:
+        filepath:  absolute file's path to load patterns from
+
+    Returns:
+        user's personal filters if existing, empty list otherwise.
+
+    """
+    if not os.path.isfile(filepath):
+        return []
+
+    return filter(lambda e: e != "",
+                  open(filepath).readlines())
+
 
 def files_from(directory, filter_files_pattern):
-    """List the files from the directory using the filter filter_files_pattern."""
-    directory_exp_filter = directory + os.sep + filter_files_pattern # filter the files of the directory
-    return glob.glob(directory_exp_filter) # load the files from the folder 'directory'
+    """List the files from the directory using filter filter_files_pattern.
 
-def rename_according_to_naming_conventions(s, user_specific_patterns):
-    """Rename according to conventions (no ',', ';', ':', ' ' or '_' replaced by '-')."""
+    Args:
+        - directory: the directory to list files from.
+        - filter_files_pattern: the filtering pattern to use.
+    """
+     # filter the files of the directory
+    directory_exp_filter = os.path.join([directory, filter_files_pattern])
+    # load the files from the folder 'directory'
+    return glob.glob(directory_exp_filter)
+
+def rename_according_to_naming_conventions(s, user_patterns):
+    """Rename according to conventions:
+      no ',', ';', ':', ' ' or '_' replaced by '-'
+
+    Args:
+        - s: string input
+        - user_patterns: user provided patterns
+
+    Returns:
+        input transformed.
+
+    """
     # 0 -> patterns source; 1 -> symbol that replace the patterns source
-    patterns = ( (user_specific_patterns, '')
+    patterns = ( (user_patterns, '')
                  ,(('          ', '        ', '       ', '     ', '    ', '   ', '  '), ' ')
                  ,((' \'', '_\'', '-\''), '\'')
                  ,((';', ':', '?', '!', ','), '')
@@ -23,16 +53,30 @@ def rename_according_to_naming_conventions(s, user_specific_patterns):
                  ,((' _ ', ' - ', ' ', '_',
                     '__________', '_________', '________', '_______', '_____', '____', '___', '__',
                     '----------', '---------', '--------', '-------', '-----', '----', '---', '--'), '-'))
-    return functools.reduce(lambda result, pattern: modify_name(result, pattern[0], pattern[1]), patterns, s.lower())
+    reduce_fn = lambda res, pattern: modify_name(res, pattern[0], pattern[1])
+    return functools.reduce(reduce_fn, patterns, s.lower())
+
 
 def modify_name(s, src_replace, dest_replace):
-    """Modify the filename : replace all the spaces by underscores and convert from upper to lower case."""
-    return functools.reduce(lambda result, symbol: result.replace(symbol, dest_replace), src_replace, s)
+    """Modify the filename: replace all the spaces by underscores
+    and convert from upper to lower case.
 
-def rename_foldername(full_old_name, user_specific_patterns, dryrun, verbose):
+    Args:
+        - s
+        - src_replace
+        - dest_replace
+
+    Returns:
+        name modified.
+    """
+    reduce_fn = lambda result, symbol: result.replace(symbol, dest_replace)
+    return functools.reduce(reduce_fn, src_replace, s)
+
+
+def rename_foldername(full_old_name, user_patterns, dryrun, verbose):
     """Rename full_old_name to new. Returns the new_name once done."""
-    (path, old_name) = os.path.split(full_old_name) # retrieve the name of the file
-    new_name = rename_according_to_naming_conventions(old_name, user_specific_patterns)
+    (path, old_name) = os.path.split(full_old_name)
+    new_name = rename_according_to_naming_conventions(old_name, user_patterns)
     if verbose: print("old_name: '" + old_name + "'\nnew name: '" + new_name + "'")
     if old_name != new_name:
         full_new_name = path + os.sep + new_name
@@ -41,42 +85,89 @@ def rename_foldername(full_old_name, user_specific_patterns, dryrun, verbose):
         return full_new_name
     return full_old_name
 
-def rename_filename(full_old_name, user_specific_patterns, dryrun, verbose):
-    """Rename full_old_name to new. Returns the new_name once done."""
-    (path, old_name) = os.path.split(full_old_name) # retrieve the name of the file
-    (old_name_without_ext, old_filename_ext) = os.path.splitext(old_name) # retrieve the name of the file
-    new_name = rename_according_to_naming_conventions(old_name_without_ext, user_specific_patterns)
+
+def rename_filename(full_old_name, user_patterns, dryrun, verbose):
+    """Rename full_old_name to new and returns the new one.
+
+    Args:
+        - full_old_name: old name to rename
+        - user_patterns: user specific patterns
+        - dryrun: do nothing
+        - verbose: verbose mode
+
+    Returns:
+        The new name
+
+    """
+    (path, old_name) = os.path.split(full_old_name)
+    (old_name_without_ext, old_filename_ext) = os.path.splitext(old_name)
+    new_name = rename_according_to_naming_conventions(old_name_without_ext,
+                                                      user_patterns)
     l = len(new_name)
-    new_name = new_name[0: l - 1 if l > 1 else l] if new_name.endswith('.') else new_name
-    if verbose: print("old_name_without_ext: '" + old_name_without_ext + "'\nnew_name_without_ext: '" + new_name + "'")
+    new_name = new_name[0: l - 1 if l > 1 else l] \
+               if new_name.endswith('.') \
+               else new_name
+
+    if verbose:
+        print("old name without ext: '%s'\nnew name without ext: '%s'" %
+              (old_name_without_ext, new_name))
+
     if old_name_without_ext != new_name:
-        full_new_name = path + os.sep + new_name + old_filename_ext.lower()
+        full_new_name = os.path.join([path, new_name, old_filename_ext.lower()])
         if not dryrun and not os.path.exists(full_new_name):
-            os.rename(full_old_name, full_new_name) # to move it at the wished destination
+            os.rename(full_old_name, full_new_name)
         return full_new_name
     return full_old_name
 
-def rename_folder_and_its_files(target, filter_files_pattern, user_specific_patterns, dryrun, verbose):
-    """Rename the folder according to conventions. Applies it to its files too."""
-    new_target = rename_foldername(target, user_specific_patterns, dryrun, verbose)
+
+def rename_folder_and_its_files(target_dir, filter_files_pattern, user_patterns,
+                                dryrun, verbose):
+    """Rename the folder according to conventions.
+    Applies it to its files too.
+
+    Args:
+        - target_dir: target directory to rename
+        - filter_files_pattern: pattern of files to filter
+        - user_patterns: some other user patterns
+        - dryrun: do nothing but show what will be done
+        - verbose: verbose mode
+
+    Returns:
+        The new name
+
+    """
+    new_target = rename_foldername(target_dir, user_patterns, dryrun, verbose)
     files = files_from(new_target, filter_files_pattern)
     for f in files:
         if os.path.isdir(f):
-            if verbose: print("Folder: " + f)
-            rename_folder_and_its_files(f, filter_files_pattern, user_specific_patterns, dryrun, verbose)
-        elif os.path.isfile(f):
-            if verbose: print("File: " + f)
-            rename_filename(f, user_specific_patterns, dryrun, verbose)
-        else:
-            if verbose: print("Not a folder nor a file, skip.")
+            if verbose:
+                print("Folder: %s" % f)
 
-def scan_and_rename(file_or_folder, filter_files_pattern, user_file_specific_patterns, recursive, dryrun, verbose):
-    """Scan and rename file_or_folder according to naming conventions."""
-    user_specific_patterns = load_user_specific_patterns_from(user_file_specific_patterns);
+            rename_folder_and_its_files(f, filter_files_pattern, user_patterns,
+                                        dryrun, verbose)
+        elif os.path.isfile(f):
+            if verbose:
+                print("File: %s" % f)
+            rename_filename(f, user_patterns, dryrun, verbose)
+        else:
+            if verbose:
+                print("Not a folder nor a file, skip.")
+
+
+def scan_and_rename(file_or_folder, filter_files_pattern,
+                    patterns_file,
+                    recursive, dryrun, verbose):
+    """Scan and rename file_or_folder's content according to naming
+    conventions.
+
+    """
+    user_patterns = load_user_specific_patterns_from(patterns_file)
+
     if recursive and os.path.isdir(file_or_folder):
-        rename_folder_and_its_files(file_or_folder, filter_files_pattern, user_specific_patterns, dryrun, verbose)
+        rename_folder_and_its_files(file_or_folder, filter_files_pattern,
+                                    user_patterns, dryrun, verbose)
     else:
         if os.path.isdir(file_or_folder):
-            rename_foldername(file_or_folder, user_specific_patterns, dryrun, verbose)
+            rename_foldername(file_or_folder, user_patterns, dryrun, verbose)
         else:
-            rename_filename(file_or_folder, user_specific_patterns, dryrun, verbose)
+            rename_filename(file_or_folder, user_patterns, dryrun, verbose)
--- utils/scan-and-rename-files.py
+++ utils/scan-and-rename-files.py
@@ -1,30 +1,64 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import os,string,glob
 import argparse
-import functools
 import files
 
 ######### parser #########
 
 # parse the arguments/options of the script
-parser = argparse.ArgumentParser(description='Process (recursively) files from a target folder to rename folder and files with convention (lower case and - separated).')
+parser = argparse.ArgumentParser(
+    description="""Process (recursively) files from a target folder to rename
+folder and files with convention (lower case and - separated).""")
+
 # optional
-parser.add_argument('-t', '--target'            , dest='target_folder_or_file'      , nargs='?', default=".", help='Target file or folder to rename. Default to current folder.')
-parser.add_argument('-f', '--filter'            , dest='filter_files_pattern'       , nargs='?', default='*', help='Filter pattern on files to apply. Default to all files.')
-parser.add_argument('-u', '--user-file-patterns', dest='user_file_specific_patterns', nargs='?', default='' , help='The user\'s file containing the specific patterns to remove when renaming filenames.')
+parser.add_argument('-t', '--target',
+                    dest='target_folder_or_file',
+                    nargs='?',
+                    default=".",
+                    help="""Target file or folder to rename.
+Default to current folder.""")
+
+parser.add_argument('-f', '--filter',
+                    dest='filter_files_pattern',
+                    nargs='?',
+                    default='*',
+                    help="""Filter pattern on files to apply.
+                    Default to all files.""")
+
+parser.add_argument('-u', '--user-file-patterns',
+                    dest='user_file_specific_patterns',
+                    nargs='?',
+                    default='' ,
+                    help="""The user's file containing the specific patterns to
+remove when renaming filenames.""")
+
 # flags
-parser.add_argument('-v', '--verbose'  , dest='verbose'  , default=False, action='store_const', const='True', help='Verbose output (default: false)')
-parser.add_argument('-r', '--recursive', dest='recursive', default=False, action='store_const', const='True', help='Recursive (default: false)')
-parser.add_argument('-n', '--dry-run',   dest='dryrun'   , default=False, action='store_const', const='True', help='Dry run (default: false)')
+parser.add_argument('-v',
+                    '--verbose',
+                    dest='verbose',
+                    default=False,
+                    action='store_const',
+                    const='True',
+                    help='Verbose output (default: false)')
+parser.add_argument('-r',
+                    '--recursive',
+                    dest='recursive',
+                    default=False,
+                    action='store_const',
+                    const='True',
+                    help='Recursive (default: false)')
+parser.add_argument('-n',
+                    '--dry-run',
+                    dest='dryrun',
+                    default=False,
+                    action='store_const',
+                    const='True',
+                    help='Dry run (default: false)')
 
 args = parser.parse_args()
-
-######### functions #########
-
-files.scan_and_rename(args.target_folder_or_file
-                      , args.filter_files_pattern
-                      , args.user_file_specific_patterns
-                      , args.recursive
-                      , args.dryrun
-                      , args.verbose)
+files.scan_and_rename(args.target_folder_or_file,
+                      args.filter_files_pattern,
+                      args.user_file_specific_patterns,
+                      args.recursive,
+                      args.dryrun,
+                      args.verbose)
