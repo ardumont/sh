24120b4b1f856a8d759dea30a0376e18c6b892e9
Antoine R. Dumont <eniotna.t@gmail.com>
2016-03-20 20:05:49 +0100

    A sample turtle script

---
+++ turtle/hello.hs
@@ -0,0 +1,10 @@
+#!/usr/bin/env stack
+ -- stack --install-ghc runghc --package turtle
+
+                                     -- #!/bin/bash
+{-# LANGUAGE OverloadedStrings #-}
+                                     --
+import           Turtle
+
+main :: IO ()                                     --
+main = echo "Hello, world!"         -- echo Hello, world!
