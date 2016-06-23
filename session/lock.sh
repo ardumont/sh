08e43c365d61ad082b71f88af8bbf2042c0a088c
Antoine R. Dumont <eniotna.t@gmail.com>
2016-06-23 14:34:02 +0200

    Try different screenlock

--- session/lock.sh
+++ session/lock.sh
@@ -2,5 +2,6 @@
 
 # Use: $0
 
-(which gnome-screensaver-command && gnome-screensaver-command --lock) || \
+(which i3lock && i3lock) || \
+    (which gnome-screensaver-command && gnome-screensaver-command --lock) || \
     (which xscreensaver-command && xscreensaver-command -lock)
